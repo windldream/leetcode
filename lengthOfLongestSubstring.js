65cfdf9b99f31c12fb98dac51022109c3eecd69f
windldream <1508353859@qq.com>
2019-04-14 14:40:59 +0800

    Longest Substring Without Repeating Characters

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -2,34 +2,51 @@
  * @param {string} s
  * @return {number}
  */
+// var lengthOfLongestSubstring = function(s) {
+//     let len = s.length, i, max = 0, j;
+
+//     if (len === 0 || len === 1) {
+//         return len;
+//     }
+
+//     for (i = 0; i < len; i += 1) {
+//         for (j = i + 1; j <= len; j += 1) {
+//             if (isUnique(s.slice(i, j))) {
+//                 max = Math.max(max, j - i);
+//             }
+//         }
+//     }
+
+//     return max;
+
+//     function isUnique(str) {
+//         let len = str.length, i = 0, map = {};
+        
+//         for (; i < len; i += 1) {
+//             if (!map[str[i]]) {
+//                 map[str[i]] = 1;
+//             } else {
+//                 return false;
+//             }
+//         }
+
+//         return true;
+//     }
+// };
+
 var lengthOfLongestSubstring = function(s) {
-    let len = s.length, i, max = 0, j;
+    let len = s.length, i = 0, max = 0, j = 0, set = new Set();
 
-    if (len === 0 || len === 1) {
-        return len;
-    }
-
-    for (i = 0; i < len; i += 1) {
-        for (j = i + 1; j <= len; j += 1) {
-            if (isUnique(s.slice(i, j))) {
-                max = Math.max(max, j - i);
-            }
+    while (i < len && j < len) {
+        if (!set.has(s[j])) {
+            set.add(s[j++]);
+            max = Math.max(max, j - i);
+        } else {
+            set.clear();
+            i++;
+            j = i;
         }
     }
 
     return max;
-
-    function isUnique(str) {
-        let len = str.length, i = 0, map = {};
-        
-        for (; i < len; i += 1) {
-            if (!map[str[i]]) {
-                map[str[i]] = 1;
-            } else {
-                return false;
-            }
-        }
-
-        return true;
-    }
 };
