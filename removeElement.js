0838c974d87fa98e75cd9ac9fe4b7c3606f011a6
windldream <1508353859@qq.com>
2019-05-08 22:20:40 +0800

    移除指定元素

---
+++ removeElement.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === val) {
+            nums.splice(i--, 1);
+        }
+    }
+    return nums.length;
+};
