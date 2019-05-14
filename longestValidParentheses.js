26c40deab3f5920bd33f1429bc8a4aea0ac053b9
windldream <1508353859@qq.com>
2019-05-14 22:09:54 +0800

    最长有效括号栈解法

--- longestValidParentheses.js
+++ longestValidParentheses.js
@@ -3,5 +3,33 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
+    let stack = [], len = s.length, i, result = [], maxLen = 0, num = 0;
+
+    for (i = 0; i < len; i++) {
+        if (s[i] === ')') {
+            if (stack.length !== 0) {
+                result.push(stack.pop())
+                result.push(i);
+            }
+        } else {
+            stack.push(i)
+        }
+    }
+
+    result.sort((a, b) => a - b);
+
+    for (i = 0; i < result.length; i++) {
+        if (result[i] === result[i + 1] - 1) {
+            num++;
+        } else {
+            maxLen = Math.max(maxLen, num);
+            num = 0;
+        }
+    }
+
+    maxLen = maxLen % 2 === 0 ? maxLen : maxLen + 1;
     
+    return maxLen;
 };
+
+longestValidParentheses('(()');
