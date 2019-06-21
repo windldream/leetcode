0e5a6593b2390d814ff8cf0a0f4b35767b3b3af4
windldream <1508353859@qq.com>
2019-06-21 15:11:47 +0800

    旋转图像

--- rotate.js
+++ rotate.js
@@ -5,20 +5,29 @@
 var rotate = function(matrix) {
     let len = matrix.length, i, j;
 
-    for (i = 0; i < len; i++) {
-        for (j = i; j < len; j++) {
-            [matrix[j][i], matrix[i][j]] = [matrix[i][j], matrix[j][i]]
+    // 顺时针交换这四个点的值(i,j) , (j, n-i-1), (n-i-1, n-j-1), (n -j-1, i)
+    for (i = 0; i < Math.ceil(len / 2); i++) {
+        for (j = i; j < len - i - 1; j++) {
+            [matrix[j][len - i - 1], matrix[i][j], matrix[len - j - 1][i], matrix[len - i - 1][len - j - 1]] = 
+                [matrix[i][j], matrix[len - j - 1][i], matrix[len - i - 1][len - j - 1], matrix[j][len - i - 1]]; 
         }
     }
-
-    console.log(matrix)
-
-    for (i = 0; i < len; i++) {
-        matrix[i].reverse()
-    }
-
-    console.log(matrix)
 };
+
+
+// var rotate = function(matrix) {
+//     let len = matrix.length, i, j;
+
+//     for (i = 0; i < len; i++) {
+//         for (j = i; j < len; j++) {
+//             [matrix[j][i], matrix[i][j]] = [matrix[i][j], matrix[j][i]]
+//         }
+//     }
+
+//     for (i = 0; i < len; i++) {
+//         matrix[i].reverse()
+//     }
+// };
 
 console.log(rotate([
     [1, 2, 3],
