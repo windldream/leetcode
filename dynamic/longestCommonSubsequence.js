189295446dd0656bb4a56b5c0b2045878e700880
“windldream” <1508353859@qq.com>
2019-12-25 22:37:46 +0800

    最长公共子序列

---
+++ dynamic/longestCommonSubsequence.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+  const m = text1.length,
+    n = text2.length,
+    dp = [];
+
+  for (let i = 0; i <= m; i++) {
+    dp[i] = [];
+    for (let j = 0; j <= n; j++) {
+      dp[i][j] = 0;
+    }
+  }
+
+  for (let i = 0; i <= m; i++) {
+    for (let j = 0; j <= n; j++) {
+      if (i === 0 || j === 0) {
+        dp[i][j] = 0;
+        // 当前位置最长子序列等于之上一个位置的最长子序列加一
+      } else if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+      }
+    }
+  }
+
+  return dp[m][n];
+};
