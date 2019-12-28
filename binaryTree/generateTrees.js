1383305c5c25252d3d07122f361951fdec94c088
“windldream” <1508353859@qq.com>
2019-12-28 19:49:34 +0800

    不同的二叉搜索树 II

--- binaryTree/generateTrees.js
+++ binaryTree/generateTrees.js
@@ -9,10 +9,13 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
+var generateTrees = function (n) {
+  if (n === 0) {
+    return [];
+  }
 
   return helper(1, n);
-  
+
   function helper(start, end) {
     let allTrees = [];
 
@@ -22,37 +25,20 @@ var generateTrees = function(n) {
     }
 
     for (let i = start; i <= end; i++) {
+
       let leftTrees = helper(start, i - 1),
         rightTrees = helper(i + 1, end);
-      
       for (let l = 0; l < leftTrees.length; l++) {
         for (let r = 0; r < rightTrees.length; r++) {
-          const arr = [i];
           let treeNode = new TreeNode(i);
           treeNode.left = leftTrees[l];
           treeNode.right = rightTrees[r];
-          bfs(treeNode, arr)
-          allTrees.push(arr);
+          allTrees.push(treeNode);
         }
       }
     }
-
     return allTrees;
-  }
-
-  function TreeNode(val) {
-    this.val = val;
-    this.left = this.right = null;
-  }
-
-  function bfs(tree, arr) {
-    if (!tree) {
-      return;
-    }
-    arr.push(tree.val);
-    bfs(tree.left, arr);
-    bfs(tree.right, arr);
   }
 };
 
-console.log(generateTrees(3));
+console.log('finall', generateTrees(3));
