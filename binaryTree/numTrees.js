0a2da0fec768bc7a3c265368b317139cd995803b
“windldream” <1508353859@qq.com>
2019-12-28 20:39:03 +0800

    不同的二叉搜索树

--- binaryTree/numTrees.js
+++ binaryTree/numTrees.js
@@ -5,8 +5,8 @@
 var numTrees = function (n) {
   let dp = [];
 
-  // dp[i] = f(i - 1) + f(n - i)
-  // dp[1] + dp[2] + ... + dp[n] = f(0) * f(n - 1) + f(1) * f(n - 2) + ... + f(n - 1) * f(0)
+  // f(i) = f(i - 1) + f(n - i)
+  // dp[n] = dp[0] * dp[n - 1] + dp[1] * dp[n - 2] + ... + dp[n - 1] * dp[0]
   dp[0] = 1;
   dp[1] = 1;
   for (let i = 2; i <= n; i++) {
