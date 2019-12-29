e720ff2f48dad73621b3f7b49b6a874c43cdb255
windldream <1508353859@qq.com>
2019-12-29 15:21:06 +0800

    不同的子序列

--- dynamic/numDistinct.js
+++ dynamic/numDistinct.js
@@ -4,8 +4,7 @@
  * @return {number}
  */
 var numDistinct = function(s, t) {
-  const len1 = s.length, len2 = t.length;
-  let res = 0;
+  const len1 = s.length, len2 = t.length, dp = [];
 
   if (len1 < len2) {
     return 0;
@@ -15,13 +14,25 @@ var numDistinct = function(s, t) {
     return s === t ? 1 : 0;
   }
 
-  for (let i = 0; i <= len1 - len2; i++) {
-    if (s[i] === t[0]) {
-      res += numDistinct(s.substring(i + 1), t.substring(1))
+  // dp[i][j] 代表T前i字符串可以由S前j字符串组成最多个数.
+  dp[0] = [];
+  for (let j = 0; j <= len1; j++) {
+    dp[0][j] = 1;
+  }
+
+  for (let i = 1; i <= len2; i++) {
+    dp[i] = [];
+    dp[i][0] = 0;
+    for (let j = 1; j <= len1; j++) {
+      if (t[i - 1] === s[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+      } else {
+        dp[i][j] = dp[i][j - 1];
+      }
     }
   }
 
-  return res;
+  return dp[len2][len1];
 };
 
 console.log(numDistinct('babgbag', 'bag'))
