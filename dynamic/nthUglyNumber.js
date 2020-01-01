fa10a519ef6b47b457a38e732ec69046d0690c20
“windldream” <1508353859@qq.com>
2020-01-01 11:41:20 +0800

    丑数 II（三指针）

--- dynamic/nthUglyNumber.js
+++ dynamic/nthUglyNumber.js
@@ -2,57 +2,30 @@
  * @param {number} n
  * @return {number}
  */
-var nthUglyNumber = function(n) {
-  let dp = [], num = 1, i = 0, cache = {};
+var nthUglyNumber = function (n) {
+  let dp = [],
+    l2 = 0,
+    l3 = 0,
+    l5 = 0;
+  dp[0] = 1;
 
-  while(true) {
-    if (i === n) {
-      return dp[n - 1]
+  for (i = 1; i < n; i++) {
+    const min = Math.min(dp[l2] * 2, dp[l3] * 3, dp[l5] * 5);
+
+    // 使用if避免相等的元素重复
+    if (min === dp[l2] * 2) {
+      l2++;
     }
-    if (isUgly(num, cache)) {
-      cache[num] = true;
-      dp[i++] = num++;
-    } else {
-      num++;
+    if (min === dp[l3] * 3) {
+      l3++;
     }
+    if (min === dp[l5] * 5) {
+      l5++;
+    }
+    dp[i] = min;
   }
 
-  function isUgly(num, cache) {
-    const uglyArr = [2, 3, 4, 5];
-    if (cache[num]) {
-      return cache[num]
-    }
-
-    if (num === 1) {
-      return true;
-    }
-  
-    while (true) {
-      if (num < 6) {
-        return uglyArr.includes(num);
-      }
-      if (Number.isInteger(num / 2)) {
-        num = num / 2;
-        if (cache[num]) {
-          return true;
-        }
-        continue;
-      } else if (Number.isInteger(num / 3)) {
-        num = num / 3;
-        if (cache[num]) {
-          return true;
-        }
-        continue;
-      } else if (Number.isInteger(num / 5)) {
-        num = num / 5;
-        if (cache[num]) {
-          return true;
-        }
-        continue;
-      }
-      return false;
-    }
-  }
+  return dp[n - 1];
 };
 
 console.log(nthUglyNumber(491))
