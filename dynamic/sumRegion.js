63dc0d8de295e2d800f1dd377494b36160e60b6f
windldream <1508353859@qq.com>
2020-01-03 14:17:27 +0800

    二维区域和检索 - 矩阵不可变

--- dynamic/sumRegion.js
+++ dynamic/sumRegion.js
@@ -2,7 +2,24 @@
  * @param {number[][]} matrix
  */
 var NumMatrix = function(matrix) {
-  this.sum = matrix;
+  if (matrix.length === 0) {
+    return;
+  }
+  if (matrix.length && matrix[0].length === 0) {
+    return;
+  }
+  if (matrix.length && matrix[0].length && matrix[0][0].length === 0) {
+    return;
+  }
+  this.sum = [];
+  let row = matrix.length, col = matrix[0].length;
+  for (let i = 0; i < row; i++) {
+    this.sum[i] = [];
+    this.sum[i][0] = 0;
+    for (j = 0; j < col; j++) {
+      this.sum[i][j + 1] = this.sum[i][j] + matrix[i][j];
+    }
+  }
 };
 
 /** 
@@ -14,16 +31,14 @@ var NumMatrix = function(matrix) {
  */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
   let sum = 0;
-  for(let i = row1; i <= row2; i++) {
-    for (let j = col1; j <= col2; j++) {
-      sum += this.sum[i][j];
-    }
+  for (let i = row1; i <= row2; i++) {
+    console.log(this.sum[i][col2 + 1], this.sum[i][col1])
+    sum += this.sum[i][col2 + 1] - this.sum[i][col1] ;
   }
   return sum;
 };
 
-/** 
- * Your NumMatrix object will be instantiated and called as such:
- * var obj = new NumMatrix(matrix)
- * var param_1 = obj.sumRegion(row1,col1,row2,col2)
- */
+let obj = new NumMatrix([[[]]]);
+
+console.log(obj.sumRegion(2, 1, 4, 3))
+
