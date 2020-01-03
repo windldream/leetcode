4e66cebc2980cd37e80fdf935fb1a4b1d66c25f1
windldream <1508353859@qq.com>
2020-01-03 11:31:12 +0800

    二维区域和检索 - 矩阵不可变（暴力解法）

---
+++ dynamic/sumRegion.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} matrix
+ */
+var NumMatrix = function(matrix) {
+  this.sum = matrix;
+};
+
+/** 
+ * @param {number} row1 
+ * @param {number} col1 
+ * @param {number} row2 
+ * @param {number} col2
+ * @return {number}
+ */
+NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
+  let sum = 0;
+  for(let i = row1; i <= row2; i++) {
+    for (let j = col1; j <= col2; j++) {
+      sum += this.sum[i][j];
+    }
+  }
+  return sum;
+};
+
+/** 
+ * Your NumMatrix object will be instantiated and called as such:
+ * var obj = new NumMatrix(matrix)
+ * var param_1 = obj.sumRegion(row1,col1,row2,col2)
+ */
