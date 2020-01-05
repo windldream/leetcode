95b81277aa40a8c36cab9b59568804d59f18a4cc
“windldream” <1508353859@qq.com>
2020-01-05 21:53:58 +0800

    矩形区域不超过 K 的最大数值和

--- dynamic/maxSumSubmatrix.js
+++ dynamic/maxSumSubmatrix.js
@@ -3,61 +3,48 @@
  * @param {number} k
  * @return {number}
  */
+// TODO 每天多看几遍该解法
 var maxSumSubmatrix = function (matrix, k) {
-  let row = matrix.length,
-    col = matrix[0].length,
-    dp = []
+  const row = matrix.length,
+    col = matrix[0].length;
+  let res = -Infinity;
 
-  for (let i = 0; i < row; i++) {
-    dp[i] = []
-    dp[i][0] = 0;
-    for (let j = 0; j < col; j++) {
-      dp[i][j + 1] = dp[i][j] + matrix[i][j]
-    }
-  }
-
-  let sum = -Infinity
-  for (let row1 = 0; row1 < row; row1++) {
-    for (let col1 = 0; col1 < col; col1++) {
-      if (row === 1) {
-        for (let col2 = col1; col2 < col; col2++) {
-          let res = getSum(row1, col1, 0, col2, dp)
-          if (res === k) {
-            return res;
-          }
-          if (res < k) {
-            sum = Math.max(sum, res)
+  for (let i = 0; i < col; i++) {
+    let rowSum = Array(row).fill(0);
+    for (let j = i; j < col; j++) {
+      let sum = 0,
+        max = -Infinity;
+      // 第r行单行矩阵的和
+      for (let r = 0; r < row; r++) {
+        rowSum[r] += matrix[r][j];
+        if (sum < 0) {
+          sum = 0;
+        }
+        sum += rowSum[r];
+        max = Math.max(max, sum);
+      }
+      if (max <= k) {
+        res = Math.max(res, max);
+      } else {
+        // 第m行到第n行之间矩阵的和
+        max = -Infinity;
+        for (let m = 0; m < row; m++) {
+          sum = 0;
+          for (let n = m; n < row; n++) {
+            sum += rowSum[n];
+            if (sum <= k) {
+              max = Math.max(max, sum);
+            }
           }
         }
-        continue;
+        res = Math.max(res, max);
       }
-      for (let row2 = row1; row2 < row; row2++) {
-        for (let col2 = col1; col2 < col; col2++) {
-          let res = getSum(row1, col1, row2, col2, dp)
-          if (res === k) {
-            return res
-          }
-          if (res < k) {
-            sum = Math.max(sum, res)
-          }
-        }
+      if (res === k) {
+        return k;
       }
     }
   }
-
-  return sum
-
-  function getSum(row1, col1, row2, col2, sums) {
-    let sum = 0
-    if (row2) {
-      for (let i = row1; i <= row2; i++) {
-        sum += sums[i][col2 + 1] - sums[i][col1]
-      }
-    } else {
-      sum = sums[row1][col2 + 1] - sums[row1][col1];
-    }
-    return sum
-  }
+  return res;
 }
 
 console.log(
