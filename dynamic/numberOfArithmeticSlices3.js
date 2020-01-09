63483ae85e22982c54b4b293e63d5461e645686e
windldream <1508353859@qq.com>
2020-01-09 16:04:40 +0800

    更新注释

--- dynamic/numberOfArithmeticSlices3.js
+++ dynamic/numberOfArithmeticSlices3.js
@@ -10,7 +10,7 @@ var numberOfArithmeticSlices = function(A) {
     dp = Array(len).fill(0).map(() => new Map());
   let count = 0;
 
-  // dp[i] 表示以 i 结尾的位置所包含的公差为 x 的等差数列的个数
+  // dp[i] 表示以 i 结尾的位置所包含的公差为 x 的弱等差数列的个数
   for (let i = 1; i < len; i++) {
     for (let j = 0; j < i;j++) {
       let x = A[i] - A[j];
@@ -20,8 +20,8 @@ var numberOfArithmeticSlices = function(A) {
         dp[i].set(x, 1);
       }
 
-      // dp[j] 表示以 j 结尾的位置所包含的公差为 x 的等差数列的个数
-      // 因此 需要更新以 i 结尾的位置的公差为 x 的等差数列的个数
+      // dp[j] 表示以 j 结尾的位置所包含的公差为 x 的弱等差数列的个数
+      // 因此 需要更新以 i 结尾的位置的公差为 x 的弱等差数列的个数
       if (dp[j].has(x)) {
         dp[i].set(x, dp[i].get(x) + dp[j].get(x));
         count += dp[j].get(x);
