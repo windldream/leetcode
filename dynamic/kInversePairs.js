834930c5dd5775e43a8bc9869f73667bd973a0b1
windldream <1508353859@qq.com>
2020-01-13 11:15:22 +0800

    K个逆序对数组

---
+++ dynamic/kInversePairs.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var kInversePairs = function(n, k) {
+  const dp = [],
+    M = 10 ** 9 + 7;
+
+  for (let i = 0; i <= n; i++) {
+    dp[i] = Array(k).fill(0);
+    for (let j = 0; j <= k; j++) {
+      if (i === 0 || i === 1) {
+        if (j === 0) {
+          dp[i][j] = 1;
+        } else {
+          dp[i][j] = 0;
+        }
+      } else if (j === 0) {
+        dp[i][j] = 1;
+      } else {
+        if (j >= i) {
+          dp[i][j] = (dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - i] + M) % M;
+        } else {
+          dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % M;
+        }
+      }
+    }
+  }
+
+  return dp[n][k];
+};
+
+console.log(kInversePairs(3, 2));
