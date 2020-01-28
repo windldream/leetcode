bb21bccfc45b532faa8d64ca98d152e9861369d5
“windldream” <1508353859@qq.com>
2020-01-28 21:46:55 +0800

    奇偶跳（动态规划）

--- dynamic/oddEvenJumps2.js
+++ dynamic/oddEvenJumps2.js
@@ -11,37 +11,35 @@ var oddEvenJumps = function(A) {
   odd[len - 1] = true;
   even[len - 1] = true;
 
-  for (let i = len - 2; i >= 0; i--) {
-    const oddEntry = Math.min.apply(
-      null,
-      A.slice(i + 1).filter(item => item >= A[i])
-    );
-    let oddIndex = -1;
-    for (let j = i + 1; j < len; j++) {
-      if (oddEntry === A[j]) {
-        oddIndex = j;
-        break;
+  for (let index = len - 2; index >= 0; index--) {
+    let min = Infinity,
+      nextIndex = -1;
+    for (let i = index + 1; i < len; i++) {
+      if (A[i] >= A[index]) {
+        if (A[i] < min) {
+          min = A[i];
+          nextIndex = i;
+        }
       }
     }
 
-    const evenEntry = Math.max.apply(
-      null,
-      A.slice(i + 1).filter(item => item <= A[i])
-    );
-    let evenIndex = -1;
-    for (let j = i + 1; j < len; j++) {
-      if (evenEntry === A[j]) {
-        evenIndex = j;
-        break;
+    let max = -Infinity,
+      nextIndex1 = -1;
+    for (let i = index + 1; i < len; i++) {
+      if (A[i] <= A[index]) {
+        if (A[i] > max) {
+          max = A[i];
+          nextIndex1 = i;
+        }
       }
     }
 
-    if (oddIndex !== -1) {
-      odd[i] = even[oddIndex];
+    if (nextIndex !== -1) {
+      odd[index] = even[nextIndex];
     }
 
-    if (evenIndex !== -1) {
-      even[i] = odd[evenIndex];
+    if (nextIndex1 !== -1) {
+      even[index] = odd[nextIndex1];
     }
   }
 
@@ -52,27 +50,6 @@ var oddEvenJumps = function(A) {
     }
   }
   return res;
-
-  function findMax(arr) {
-    if (arr.length === 0) {
-      return arr;
-    }
-    const len = arr.length,
-      res = Array(len),
-      stack = [];
-    stack.push(0);
-    let index = 1;
-    while (index < len) {
-      while (stack.length && arr[index] > stack[stack.length - 1]) {
-        res[stack.pop()] = index;
-      }
-      stack.push(index++);
-    }
-    if (stack.length) {
-      res[stack.pop()] = -1;
-    }
-    return res;
-  }
 };
 
 console.log(oddEvenJumps([10, 13, 12, 14, 15]));
