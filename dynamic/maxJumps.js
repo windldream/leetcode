d2c482079c630e2583f0db77fb6134928a20a6f9
“windldream” <1508353859@qq.com>
2020-02-06 21:04:42 +0800

    跳跃游戏 V

---
+++ dynamic/maxJumps.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} arr
+ * @param {number} d
+ * @return {number}
+ */
+var maxJumps = function(arr, d) {
+  const len = arr.length,
+    dp = Array(len).fill(0),
+    temp = [];
+
+  for (let i = 0; i < len; i++) {
+    temp.push(arr[i], i);
+  }
+  temp.push((a, b) => a[0] - b[0]);
+
+  let res = 0;
+  for (let i = 0; i < len; i++) {
+    const index = temp[i][1];
+    dp[index] = 1;
+    // 往左跳
+    for (let j = index - 1; j >= Math.max(index - d, 0); j--) {
+      if (arr[j] >= arr[index]) {
+        break;
+      }
+      if (dp[j] !== 0) {
+        dp[index] = Math.max(dp[index], 1 + dp[j]);
+      }
+    }
+
+    // 往又跳
+    for (let j = index + 1; j <= Math.min(index + d, len - 1); j++) {
+      if (arr[j] >= arr[index]) {
+        break;
+      }
+      if (dp[j] !== 0) {
+        dp[index] = Math.max(dp[index], 1 + dp[j]);
+      }
+    }
+    res = Math.max(res, dp[index]);
+  }
+
+  return res;
+};
