fc8c96bb37f5876c27f892296e0c363cdab56a99
“windldream” <1508353859@qq.com>
2020-02-09 22:48:42 +0800

    柠檬水找零

--- greedy/lemonadeChange.js
+++ greedy/lemonadeChange.js
@@ -3,31 +3,26 @@
  * @return {boolean}
  */
 var lemonadeChange = function(bills) {
-  const map = {
-      '5': 0,
-      '10': 0,
-      '20': 0
-    },
-    len = bills.length;
+  const len = bills.length;
+  let five = 0,
+    ten = 0;
 
   for (let i = 0; i < len; i++) {
     if (bills[i] === 5) {
-      map[5] += 1;
+      five += 1;
     } else if (bills[i] === 10) {
-      if (!map[5]) {
+      if (!five) {
         return false;
       } else {
-        map[5] -= 1;
-        map[10] += 1;
+        five -= 1;
+        ten += 1;
       }
     } else if (bills[i] === 20) {
-      if (map[10] && map[5]) {
-        map[10] -= 1;
-        map[5] -= 1;
-        map[20] += 1;
-      } else if (map[5] >= 3) {
-        map[5] -= 3;
-        map[20] += 1;
+      if (ten && five) {
+        ten -= 1;
+        five -= 1;
+      } else if (five >= 3) {
+        five -= 3;
       } else {
         return false;
       }
