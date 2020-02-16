a793718e1637301b4127c3abc5911d3172a87add
“windldream” <1508353859@qq.com>
2020-02-16 10:46:01 +0800

    二叉树的最近公共祖先

---
+++ lowestCommonAncestor2.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (root === null || root === p || root === q) {
+    return root;
+  }
+  const l = lowestCommonAncestor(root.left, p, q);
+  const r = lowestCommonAncestor(root.right, p, q);
+
+  if (l !== null && r !== null) {
+    return root;
+  } else if (l != null) {
+    return l;
+  } else if (r !== null) {
+    return r;
+  }
+
+  return null;
+};
