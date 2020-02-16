70d76cffb0ec190c80502f2573a81a73f0414d53
“windldream” <1508353859@qq.com>
2020-02-16 16:31:51 +0800

    序列化和反序列化二叉搜索树

---
+++ tree/serialize2.js
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * Encodes a tree to a single string.
+ *
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var serialize = function(root) {
+  const res = [];
+  helper(root);
+  return JSON.stringify(res);
+
+  function helper(root) {
+    if (root === null) {
+      res.push(null);
+      return;
+    }
+    res.push(root.val);
+    helper(root.left);
+    helper(root.right);
+  }
+};
+
+/**
+ * Decodes your encoded data to tree.
+ *
+ * @param {string} data
+ * @return {TreeNode}
+ */
+var deserialize = function(data) {
+  data = JSON.parse(data);
+  if (data.length === 0) {
+    return null;
+  }
+  return rebuild(data);
+
+  function rebuild(data) {
+    if (data[0] === null) {
+      data.shift();
+      return null;
+    }
+    const root = new TreeNode(data[0]);
+    data.shift();
+    root.left = rebuild(data);
+    root.right = rebuild(data);
+    return root;
+  }
+};
+
+/**
+ * Your functions will be called as such:
+ * deserialize(serialize(root));
+ */
