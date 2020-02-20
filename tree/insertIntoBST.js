942b802c1ad6b2e95f2f8ad1a53d585d1f5ad5ee
“windldream” <1508353859@qq.com>
2020-02-20 20:37:09 +0800

    二叉搜索树中的插入操作

---
+++ tree/insertIntoBST.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function(root, val) {
+  if (root === null) {
+    return new TreeNode(val);
+  }
+
+  if (root.val > val) {
+    root.left = insertIntoBST(root.left, val);
+  } else {
+    root.right = insertIntoBST(root.right, val);
+  }
+
+  return root;
+};
