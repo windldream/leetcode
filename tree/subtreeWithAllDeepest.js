510bf0775ca6e58ee6f10980f2a19aed4bff95a8
“windldream” <1508353859@qq.com>
2020-02-21 21:24:42 +0800

    具有所有最深结点的最小子树

---
+++ tree/subtreeWithAllDeepest.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var subtreeWithAllDeepest = function(root) {
+  if (root === null) {
+    return null;
+  }
+  const l = getDepth(root.left);
+  const r = getDepth(root.right);
+  if (l === r) {
+    return root;
+  } else if (l < r) {
+    return subtreeWithAllDeepest(root.right);
+  } else {
+    return subtreeWithAllDeepest(root.left);
+  }
+
+  function getDepth(root) {
+    if (root === null) {
+      return 0;
+    }
+    return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+  }
+};
