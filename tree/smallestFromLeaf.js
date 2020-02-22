e75b9d1a6d8fed96372aebde4923e262c762d5e5
“windldream” <1508353859@qq.com>
2020-02-22 20:20:38 +0800

    从叶结点开始的最小字符串

--- tree/smallestFromLeaf.js
+++ tree/smallestFromLeaf.js
@@ -10,3 +10,47 @@
  * @return {string}
  */
 var smallestFromLeaf = function(root) {};
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var smallestFromLeaf = function(root) {
+  if (root === null) {
+    return '';
+  }
+  const codeMap = [],
+    a = 'a'.charCodeAt();
+  for (let i = 0; i <= 25; i++) {
+    const code = String.fromCharCode(i + a);
+    codeMap[i] = code;
+  }
+  const res = [];
+  dfs(root, '');
+  res.sort();
+  return res[0];
+
+  function dfs(root, path) {
+    if (root === null) {
+      return;
+    }
+    path += codeMap[root.val];
+    if (root.left === null && root.right === null) {
+      res.push(
+        path
+          .split('')
+          .reverse()
+          .join('')
+      );
+      return;
+    }
+    dfs(root.left, path);
+    dfs(root.right, path);
+  }
+};
