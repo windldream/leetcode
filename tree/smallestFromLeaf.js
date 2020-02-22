5c8cea1c094574afa6d26faab6ae032e6fb632b1
“windldream” <1508353859@qq.com>
2020-02-22 20:20:03 +0800

    从叶结点开始的最小字符串

---
+++ tree/smallestFromLeaf.js
@@ -0,0 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string}
+ */
+var smallestFromLeaf = function(root) {};
