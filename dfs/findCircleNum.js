1e4b4a7162175e1d173b98718a1ebda5b7c12348
“windldream” <1508353859@qq.com>
2020-02-27 21:50:21 +0800

    547. 朋友圈

---
+++ dfs/findCircleNum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} M
+ * @return {number}
+ */
+var findCircleNum = function(M) {
+  const r = M.length,
+    c = M[0].length,
+    visited = [];
+  let count = 0;
+  for (let i = 0; i < r; i++) {
+    if (!visited[i]) {
+      dfs(visited, i);
+      count++;
+    }
+  }
+  return count;
+
+  function dfs(visited, i) {
+    for (let j = 0; j < c; j++) {
+      if (M[i][j] && !visited[j]) {
+        visited[j] = 1;
+        dfs(visited, j);
+      }
+    }
+  }
+};
