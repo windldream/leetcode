1045232f506b56db3d56147fdbd9c7f6be09103c
“windldream” <1508353859@qq.com>
2020-03-10 21:35:49 +0800

    1319. 连通网络的操作次数

---
+++ dfs/makeConnected.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number} n
+ * @param {number[][]} connections
+ * @return {number}
+ */
+var makeConnected = function(n, connections) {
+  const len = connections.length;
+  const hashMap = Array(len)
+    .fill(0)
+    .map((val, index) => index);
+  if (len < n - 1) {
+    return -1;
+  }
+
+  for (let [u, v] of connections) {
+    union(u, v);
+  }
+
+  const set = new Set();
+  for (let i = 0; i < n; i++) {
+    set.add(find(i));
+  }
+  return set.size - 1;
+
+  function find(x) {
+    let r = x;
+    while (hashMap[r] != r) {
+      r = hashMap[r];
+    }
+    return r;
+  }
+
+  function union(x, y) {
+    const fx = find(x);
+    const fy = find(y);
+    if (fx != fy) {
+      hashMap[fx] = fy;
+    }
+  }
+};
