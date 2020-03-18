58901380ca48972fe0cfee9b7b6af76dce5056ad
“windldream” <1508353859@qq.com>
2020-03-18 21:43:19 +0800

    1091. 二进制矩阵中的最短路径

---
+++ bfs/shortestPathBinaryMatrix.js
@@ -0,0 +1,48 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var shortestPathBinaryMatrix = function(grid) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (grid[0][0] === 1 || grid[m - 1][n - 1] === 1) {
+    return -1;
+  }
+  const dir = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+    [-1, -1],
+    [-1, 1],
+    [1, -1],
+    [1, 1]
+  ];
+  const queue = [];
+  queue.push([0, 0]);
+
+  let step = 0;
+  while (queue.length) {
+    step++;
+    for (let len = queue.length - 1; len >= 0; len--) {
+      const [i, j] = queue.shift();
+      if (i === grid.length - 1 && j === grid[0].length - 1) {
+        return step;
+      }
+      for (const [r, c] of dir) {
+        const m = i + r;
+        const n = j + c;
+        if (
+          m < 0 ||
+          n < 0 ||
+          m >= grid.length ||
+          n >= grid[0].length ||
+          grid[m][n] === 1
+        )
+          continue;
+        queue.push([m, n]);
+      }
+    }
+  }
+  return -1;
+};
