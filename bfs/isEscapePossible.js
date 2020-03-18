edac79349eb9c8d82d4a578e0bc536eb806e2188
“windldream” <1508353859@qq.com>
2020-03-18 19:28:53 +0800

    1036. 逃离大迷宫（优化解法）

--- bfs/isEscapePossible.js
+++ bfs/isEscapePossible.js
@@ -25,47 +25,42 @@ var isEscapePossible = function(blocked, source, target) {
     if (startQueue.length > blockLen && endQueue.length > blockLen) {
       return true;
     }
-    for (let i = startQueue.length - 1; i >= 0; i--) {
-      const [r, c] = startQueue.shift();
-      if (endQueue.some(([i, j]) => i === r && j === c)) {
-        return true;
-      }
-      addNode(startQueue, r, c, startVisited);
+    if (
+      endQueue.some(([i, j]) => startQueue.some(([m, n]) => i === m && j === n))
+    ) {
+      return true;
     }
-
-    for (let i = endQueue.length - 1; i >= 0; i--) {
-      const [r, c] = endQueue.shift();
-      if (startQueue.some(([i, j]) => i === r && j === c)) {
-        return true;
-      }
-      addNode(endQueue, r, c, endVisited);
-    }
+    addNode(startQueue, startVisited);
+    addNode(endQueue, endVisited);
   }
 
   return false;
 
-  function addNode(queue, r, c, visited) {
+  function addNode(queue, visited) {
     const dir = [
       [0, 1],
       [0, -1],
       [-1, 0],
       [1, 0]
     ];
-    for (let d = 0; d < 4; d++) {
-      const m = dir[d][0] + r;
-      const n = dir[d][1] + c;
-      if (
-        m < 0 ||
-        n < 0 ||
-        m >= M ||
-        n >= M ||
-        blocked.some(([i, j]) => i === m && j === n) ||
-        visited.has(m + '$' + n)
-      ) {
-        continue;
+    for (let i = queue.length - 1; i >= 0; i--) {
+      const [r, c] = queue.shift();
+      for (let d = 0; d < 4; d++) {
+        const m = dir[d][0] + r;
+        const n = dir[d][1] + c;
+        if (
+          m < 0 ||
+          n < 0 ||
+          m >= M ||
+          n >= M ||
+          blocked.some(([i, j]) => i === m && j === n) ||
+          visited.has(m + '$' + n)
+        ) {
+          continue;
+        }
+        queue.push([m, n]);
+        visited.add(m + '$' + n);
       }
-      queue.push([m, n]);
-      visited.add(m + '$' + n);
     }
   }
 };
