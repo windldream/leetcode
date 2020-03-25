7a630fdf7c7a7965717a2014bf22de030191d119
windldream <1508353859@qq.com>
2020-03-25 17:41:00 +0800

    350. 两个数组的交集 II

--- sort/intersection.js
+++ sort/intersection.js
@@ -4,5 +4,13 @@
  * @return {number[]}
  */
 var intersection = function (nums1, nums2) {
-  return Array.from(new Set(nums1.filter(val => nums2.includes(val))));
+  return nums1.filter((val) => {
+    let index = nums2.indexOf(val);
+    if (index > -1) {
+      nums2.splice(index, 1);
+      return true;
+    } else {
+      return false;
+    }
+  });
 };
