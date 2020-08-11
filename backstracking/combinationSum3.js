e22dea41b5e81dc6c0c69a85c8976bb19e73c9be
“windldream” <1508353859@qq.com>
2020-08-11 22:45:59 +0800

    216. 组合总和 III

--- backstracking/combinationSum3.js
+++ backstracking/combinationSum3.js
@@ -5,24 +5,19 @@
  */
 var combinationSum3 = function (k, n) {
   const res = []
-  const set = new Set()
-  backtrack([], k)
+  backtrack([], k, 1, n)
   return res
 
-  function backtrack(track, k) {
-    const sum = track.reduce((pre, cur) => pre + cur, 0)
-    if (sum > n) return
-    if (sum === n && k === 0) {
-      const copy = track.slice()
-      if (set.has(copy.sort((a, b) => a - b).join(''))) return
-      res.push(copy)
-      set.add(copy.join(''))
+  function backtrack(track, k, start, n) {
+    if (n < 0) return
+    if (k === 0) {
+      if (n === 0) res.push(track.slice())
       return
     }
-    for (let i = 1; i <= 9; i++) {
+    for (let i = start; i <= 9; i++) {
       if (track.includes(i)) continue
       track.push(i)
-      backtrack(track, k - 1)
+      backtrack(track, k - 1, i + 1, n - i)
       track.pop()
     }
   }
