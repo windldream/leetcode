f77d5f24994848f396a254746bb0cfa9c27b5b25
“windldream” <1508353859@qq.com>
2020-08-19 19:48:55 +0800

    496. 下一个更大元素 I

--- stack/nextGreaterElement.js
+++ stack/nextGreaterElement.js
@@ -4,16 +4,15 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
-  const res = []
-  label: for (let i = 0; i < nums1.length; i++) {
-    const index = nums2.indexOf(nums1[i])
-    for (let j = index + 1; j < nums2.length; j++) {
-      if (nums2[j] > nums1[i]) {
-        res.push(nums2[j])
-        continue label
-      }
+  const stack = []
+  const map = new Map()
+  for (let i = nums2.length - 1; i >= 0; i--) {
+    while (stack.length && stack[stack.length - 1] < nums2[i]) {
+      map.set(stack.pop(), nums2[i])
     }
-    res.push(-1)
+    stack.push(nums2[i])
   }
-  return res
+
+  console.log(stack, map)
+  return nums1.map((num) => (map.has(num) && map.get(num)) || -1)
 }
