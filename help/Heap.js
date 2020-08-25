f35d58a41400cc26cdb242119c1fc7482dbe978b
“windldream” <1508353859@qq.com>
2020-08-25 20:47:01 +0800

    构造堆

---
+++ help/Heap.js
@@ -0,0 +1,58 @@
+class Heap {
+  constructor(compare) {
+    this.heap = []
+    this.size = 0
+    this.compare =
+      compare ||
+      function (a, b) {
+        return a - b > 0
+      }
+  }
+
+  insert(item) {
+    this.heap.push(item)
+    this.size += 1
+    this.up(this.size - 1)
+  }
+
+  remove() {
+    this.swap(this.size - 1, 0)
+    this.size -= 1
+    this.heap.length -= 1
+    this.down(0)
+  }
+
+  down(k) {
+    let left = k * 2 + 1,
+      right = k * 2 + 2,
+      largest = k
+
+    if (left < this.size && this.compare(this.heap[left], this.heap[largest])) {
+      largest = left
+    }
+
+    if (right < this.size && this.compare(this.heap[right], this.heap[largest])) {
+      largest = right
+    }
+
+    if (largest !== k) {
+      this.swap(k, largest)
+      this.down(largest)
+    }
+  }
+
+  up(k) {
+    let parent = Math.floor((k - 1) / 2)
+    while (k > 0 && this.compare(this.heap[k], this.heap[parent])) {
+      this.swap(k, parent)
+      k = parent
+      parent = Math.floor((parent - 1) / 2)
+    }
+  }
+
+  swap(i, j) {
+    const tmp = this.heap[i]
+    this.heap[i] = this.heap[j]
+    this.heap[j] = tmp
+  }
+}
