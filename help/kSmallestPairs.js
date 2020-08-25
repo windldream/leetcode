73b80d4ad7021ba41a504948df334c606d6e9b53
“windldream” <1508353859@qq.com>
2020-08-25 21:16:04 +0800

    373. 查找和最小的K对数字

--- help/kSmallestPairs.js
+++ help/kSmallestPairs.js
@@ -58,3 +58,29 @@ class Heap {
     this.heap[j] = tmp
   }
 }
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function (nums1, nums2, k) {
+  const minHeap = new Heap((a, b) => a[0] + a[1] < b[0] + b[1])
+  for (let i = 0; i < nums1.length; i++) {
+    for (let j = 0; j < nums2.length; j++) {
+      minHeap.insert([nums1[i], nums2[j]])
+    }
+  }
+
+  if (minHeap.size < k) {
+    return minHeap.heap
+  }
+
+  const res = []
+  while (k > 0) {
+    res.push(minHeap.remove())
+    k--
+  }
+  return res
+}
