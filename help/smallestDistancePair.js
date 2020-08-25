8d56695d67a97cc67e53322f5d7a200d30461de5
“windldream” <1508353859@qq.com>
2020-08-25 22:52:27 +0800

    719. 找出第 k 小的距离对

---
+++ help/smallestDistancePair.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestDistancePair = function (nums, k) {
+  nums.sort((a, b) => a - b)
+
+  let lo = 0
+  let hi = nums[nums.length - 1]
+  while (lo < hi) {
+    let mi = Math.floor((lo + hi) / 2)
+    let count = 0
+    let left = 0
+    for (let right = 0; right < nums.length; right++) {
+      while (nums[right] - nums[left] > mi) left++
+      count += right - left
+    }
+    if (count >= k) {
+      hi = mi
+    } else {
+      lo = mi + 1
+    }
+  }
+  return lo
+}
