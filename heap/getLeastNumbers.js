3e2885205c44e2b21e76ee2c580a9e4721bb540b
“windldream” <1508353859@qq.com>
2020-08-27 20:18:37 +0800

    剑指 Offer 40. 最小的k个数

---
+++ heap/getLeastNumbers.js
@@ -0,0 +1,81 @@
+class Heap {
+  constructor(compare = (a, b) => a - b > 0) {
+    this.heap = []
+    this.compare = compare
+  }
+
+  size() {
+    return this.heap.length
+  }
+
+  top() {
+    return this.heap[0]
+  }
+
+  insert(item) {
+    this.heap.push(item)
+    this.up(this.size() - 1)
+  }
+
+  remove() {
+    const delItem = this.heap[0]
+    this.swap(this.size() - 1, 0)
+    this.heap.length -= 1
+    this.down(0)
+    return delItem
+  }
+
+  down(k) {
+    let left = k * 2 + 1,
+      right = k * 2 + 2,
+      largest = k
+
+    if (left < this.size() && this.compare(this.heap[left], this.heap[largest])) {
+      largest = left
+    }
+
+    if (right < this.size() && this.compare(this.heap[right], this.heap[largest])) {
+      largest = right
+    }
+
+    if (largest !== k) {
+      this.swap(k, largest)
+      this.down(largest)
+    }
+  }
+
+  up(k) {
+    let parent = Math.floor((k - 1) / 2)
+    while (k > 0 && this.compare(this.heap[k], this.heap[parent])) {
+      this.swap(k, parent)
+      k = parent
+      parent = Math.floor((parent - 1) / 2)
+    }
+  }
+
+  swap(i, j) {
+    const tmp = this.heap[i]
+    this.heap[i] = this.heap[j]
+    this.heap[j] = tmp
+  }
+}
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var getLeastNumbers = function (arr, k) {
+  const maxHeap = new Heap()
+  for (let i = 0; i < arr.length; i++) {
+    if (maxHeap.size() < k) {
+      maxHeap.insert(arr[i])
+    } else {
+      if (maxHeap.top() > arr[i]) {
+        maxHeap.remove()
+        maxHeap.insert(arr[i])
+      }
+    }
+  }
+  return maxHeap.heap
+}
