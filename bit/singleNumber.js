c0554798e3a6cc34b7f5bc80fb2822ce3705ac38
“windldream” <1508353859@qq.com>
2020-08-27 21:09:06 +0800

    136. 只出现一次的数字

--- bit/singleNumber.js
+++ bit/singleNumber.js
@@ -3,15 +3,9 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-  const map = new Map()
+  let res = 0
   for (const num of nums) {
-    if (!map.has(num)) {
-      map.set(num, 0)
-    }
-    map.set(num, map.get(num) + 1)
+    res ^= num
   }
-
-  for (const [key, val] of map) {
-    if (val === 1) return key
-  }
+  return res
 }
