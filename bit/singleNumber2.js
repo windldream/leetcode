fe9da1c91546a9d3568236858c760ac2a17b229d
“windldream” <1508353859@qq.com>
2020-08-27 22:57:32 +0800

    137. 只出现一次的数字 II

--- bit/singleNumber2.js
+++ bit/singleNumber2.js
@@ -3,15 +3,18 @@
  * @return {number}
  */
 var singleNumber = function (nums) {
-  const map = new Map()
-  for (const num of nums) {
-    if (!map.has(num)) {
-      map.set(num, 0)
+  let res = 0
+  for (let i = 0; i < 32; i++) {
+    let mask = 1 << i
+    let count = 0
+    for (let j = 0; j < nums.length; j++) {
+      if ((nums[j] & mask) !== 0) {
+        count++
+      }
     }
-    map.set(num, map.get(num) + 1)
+    if (count % 3 !== 0) {
+      res |= mask
+    }
   }
-
-  for (const [key, val] of map) {
-    if (val === 1) return key
-  }
+  return res
 }
