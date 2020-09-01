bbd511c5c3ea0b1e86c98de7856e0322457629a1
“windldream” <1508353859@qq.com>
2020-09-01 18:26:26 +0800

    713. 乘积小于K的子数组

--- twoPointers/numSubarrayProductLessThanK.js
+++ twoPointers/numSubarrayProductLessThanK.js
@@ -6,24 +6,19 @@
 var numSubarrayProductLessThanK = function (nums, k) {
   let l = 0
   let r = 0
-  let arr = []
   let sum = 1
   let ans = 0
   while (r < nums.length) {
     if (nums[r] >= k) {
       l = ++r
       sum = 1
-      arr.length = 0
       continue
     }
-    arr.push(nums[r])
     sum *= nums[r]
     while (sum >= k) {
       sum /= nums[l++]
-      arr.shift()
     }
-    ans++
-    ans += arr.length - 1
+    ans += r - l + 1
     r++
   }
   return ans
