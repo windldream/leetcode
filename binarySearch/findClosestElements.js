87d9b4712a8dfd97cf3e352fd3160d4c42fe3ad5
“windldream” <1508353859@qq.com>
2020-09-05 09:57:09 +0800

    658. 找到 K 个最接近的元素

--- binarySearch/findClosestElements.js
+++ binarySearch/findClosestElements.js
@@ -5,14 +5,17 @@
  * @return {number[]}
  */
 var findClosestElements = function (arr, k, x) {
-  arr.sort((a, b) => {
-    const diff = Math.abs(a - x) - Math.abs(b - x)
-    if (diff === 0) {
-      return a - b
+  let l = 0
+  let r = arr.length - 1
+  let removeNum = arr.length - k
+  while (removeNum > 0) {
+    if (x - arr[l] <= arr[r] - x) {
+      r--
     } else {
-      return diff
+      l++
     }
-  })
+    removeNum--
+  }
 
-  return arr.slice(0, k).sort((a, b) => a - b)
+  return arr.slice(l, l + k)
 }
