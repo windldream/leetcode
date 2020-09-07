e43f72148cd4a522fc1a901a6ecfe27c0606ca2d
“windldream” <1508353859@qq.com>
2020-09-08 01:19:55 +0800

    1562. 查找大小为 M 的最新分组

--- binarySearch/findLatestStep.js
+++ binarySearch/findLatestStep.js
@@ -5,32 +5,27 @@
  */
 var findLatestStep = function (arr, m) {
   const len = arr.length
-  const res = Array(len).fill(1)
-  for (let i = len; i >= 1; i--) {
-    if (check(res, m)) return i
-    res[arr[i - 1] - 1] = 0
+  if (len === m) return m
+  const res = Array(len + 1).fill(1)
+  for (let i = len - 1; i >= 0; i--) {
+    res[arr[i]] = 0
+    if (check(res, arr[i], m)) return i
   }
   return -1
 
-  function check(arr, m) {
-    const len = arr.length
-    let i = 0
-    while (i < len) {
-      if (arr[i] === 1 && arr[i - 1] !== 1) {
-        let j = i
-        let count = m
-        while (arr[j] === 1 && count > 0) {
-          j++
-          count--
-        }
-        if (count === 0) {
-          if (arr[j] !== 1) return true
-        }
-        i = j
-      } else {
-        i++
-      }
+  function check(res, index, m) {
+    let i = index - 1
+    let j = index + 1
+    let l = 0
+    let r = 0
+    while (i >= 1 && res[i] !== 0 && l <= m) {
+      l++
+      i--
     }
-    return false
+    while (j < res.length && res[j] !== 0 && r <= m) {
+      r++
+      j++
+    }
+    return l === m || r === m
   }
 }
