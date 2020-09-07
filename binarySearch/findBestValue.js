cb6229a6912e9b7b03df4685b2dc2f97aaf25e01
“windldream” <1508353859@qq.com>
2020-09-07 20:21:16 +0800

    1300. 转变数组后最接近目标值的数组和

--- binarySearch/findBestValue.js
+++ binarySearch/findBestValue.js
@@ -4,30 +4,23 @@
  * @return {number}
  */
 var findBestValue = function (arr, target) {
-  let lo = 1
-  let hi = Math.max.apply(null, arr) + 1
-  while (lo < hi) {
-    const mid = lo + ((hi - lo) >> 1)
-    const total = getTotal(arr, mid)
-    if (total < target) {
-      lo = mid + 1
-    } else {
-      hi = mid
+  arr.sort((a, b) => a - b)
+  let sum = 0
+  const n = arr.length
+  for (let i = 0; i < arr.length; i++) {
+    const x = Math.trunc((target - sum) / (n - i))
+    if (x < arr[i]) {
+      const y = (target - sum) / (n - i)
+      if (y - x > 0.5) {
+        return x + 1
+      } else {
+        return x
+      }
     }
+    sum += arr[i]
   }
 
-  const sum1 = getTotal(arr, lo - 1)
-  const sum2 = getTotal(arr, lo)
-  if (Math.abs(target - sum1) <= Math.abs(sum2 - target)) {
-    return lo - 1
-  }
-  return lo
-
-  function getTotal(arr, target) {
-    return arr.reduce((pre, cur) => {
-      return pre + (cur > target ? target : cur)
-    }, 0)
-  }
+  return arr[arr.length - 1]
 }
 
 findBestValue([2, 3, 5], 11)
