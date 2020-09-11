183fae375283b3b4dbbf9490015d4d93f20b371a
“windldream” <1508353859@qq.com>
2020-09-11 23:30:12 +0800

    203. 移除链表元素

---
+++ linkedList/removeElements.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  if (head === null) return head
+  head.next = removeElements(head.next, val)
+  return head.val === val ? head.next : head
+}
