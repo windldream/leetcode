c4a8fdf321c5ec04a454295d88e26d11f7cc4ae1
“windldream” <1508353859@qq.com>
2020-09-12 19:46:32 +0800

    剑指 Offer 24. 反转链表

---
+++ linkedList/reverseList2.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  if (head === null || head.next === null) return head
+  const last = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return last
+}
