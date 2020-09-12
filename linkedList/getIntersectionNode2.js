c0e6ac5bff567cbf0b28237433226b2598bc4e6a
“windldream” <1508353859@qq.com>
2020-09-12 21:04:53 +0800

    面试题 02.07. 链表相交

---
+++ linkedList/getIntersectionNode2.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) return null
+
+  let a = headA
+  let b = headB
+  while (a !== b) {
+    a = a === null ? headB : a.next
+    b = b === null ? headA : b.next
+  }
+  return a
+}
