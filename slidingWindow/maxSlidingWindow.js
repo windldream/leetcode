388d476871ad51234911df0f05db02b7dc77a0ff
“windldream” <1508353859@qq.com>
2020-09-13 23:36:53 +0800

    剑指 Offer 59 - I. 滑动窗口的最大值

--- slidingWindow/maxSlidingWindow.js
+++ slidingWindow/maxSlidingWindow.js
@@ -7,16 +7,20 @@ var maxSlidingWindow = function (nums, k) {
   const len = nums.length
   if (len === 0) return []
 
-  let r = 0
-  let ans = []
-  const arr = []
-  while (r < len) {
-    arr.push(nums[r])
-    if (arr.length === k) {
-      ans.push(Math.max.apply(null, arr))
-      arr.shift()
+  const stack = []
+  const ans = []
+  for (let i = 0; i < len; i++) {
+    // 单调递减的栈
+    while (stack.length && nums[stack[stack.length - 1]] < nums[i]) {
+      stack.pop()
     }
-    r++
+    stack.push(i)
+    if (stack[0] <= i - k) {
+      stack.shift()
+    }
+    if (i - k + 1 >= 0) {
+      ans.push(nums[stack[0]])
+    }
   }
   return ans
 }
