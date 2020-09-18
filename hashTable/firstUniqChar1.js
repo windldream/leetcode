b2232d06fe3973c7cf4e989dc9a32331f0c23f44
“windldream” <1508353859@qq.com>
2020-09-18 19:48:49 +0800

    剑指 Offer 50. 第一个只出现一次的字符

--- hashTable/firstUniqChar1.js
+++ hashTable/firstUniqChar1.js
@@ -3,18 +3,8 @@
  * @return {character}
  */
 var firstUniqChar = function (s) {
-  const len = s.length
-  if (len === 0) return ' '
-  const map = new Map()
   for (const c of s) {
-    if (!map.has(c)) {
-      map.set(c, 0)
-    }
-    map.set(c, map.get(c) + 1)
-  }
-
-  for (const c of s) {
-    if (map.get(c) === 1) return c
+    if (s.indexOf(c) === s.lastIndexOf(c)) return c
   }
   return ' '
 }
