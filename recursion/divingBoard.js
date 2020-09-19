6c3125517720f5ac30db98f59d72528796a8cacc
“windldream” <1508353859@qq.com>
2020-09-19 23:33:30 +0800

    面试题 16.11. 跳水板

--- recursion/divingBoard.js
+++ recursion/divingBoard.js
@@ -8,20 +8,9 @@ var divingBoard = function (shorter, longer, k) {
   if (k === 0) return []
   if (shorter === longer) return [shorter * k]
 
-  const set = new Set()
-  const visited = new Set()
-  recursion(shorter, longer, k, 0, visited, set)
-  return [...set]
-
-  function recursion(shorter, longer, k, len, visited, set) {
-    if (k === 0) {
-      set.add(len)
-      return
-    }
-    const key = k + ' ' + len
-    if (visited.has(key)) return
-    recursion(shorter, longer, k - 1, len + shorter, visited, set)
-    recursion(shorter, longer, k - 1, len + longer, visited, set)
-    visited.add(key)
+  const ans = Array(k + 1).fill(0)
+  for (let i = 0; i <= k; i++) {
+    ans[i] = shorter * (k - i) + longer * i
   }
+  return ans
 }
