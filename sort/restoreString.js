bb45aed62658ee81c16795e9aad289b2ccdb2219
“windldream” <1508353859@qq.com>
2020-09-29 23:23:30 +0800

    1528. 重新排列字符串

---
+++ sort/restoreString.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+  const ans = []
+  for (let i = 0; i < s.length; i++) {
+    ans[indices[i]] = s[i]
+  }
+  return ans.join('')
+}
