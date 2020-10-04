e726702a1b15f2d1970e1c742b91241f9b3ff662
“windldream” <1508353859@qq.com>
2020-10-04 13:35:11 +0800

    1494. 并行课程 II

---
+++ graph/minNumberOfSemesters.js
@@ -0,0 +1,56 @@
+/**
+ * @param {number} n
+ * @param {number[][]} dependencies
+ * @param {number} k
+ * @return {number}
+ */
+var minNumberOfSemesters = function (n, dependencies, k) {
+  const pre = Array(n).fill(0)
+  for (let [u, v] of dependencies) {
+    pre[--u] |= 1 << --v
+  }
+
+  const all = 1 << n
+  const dp = Array(all).fill(n)
+  dp[0] = 0
+  for (let state = 0; state < all; state++) {
+    let next = 0
+    for (let i = 0; i < n; i++) {
+      if ((state & pre[i]) === pre[i]) {
+        next |= 1 << i
+      }
+    }
+    next &= ~state
+    for (let sub = next; sub > 0; sub = (sub - 1) & next) {
+      if (countBit(sub) <= k) {
+        dp[state | sub] = Math.min(dp[state | sub], dp[state] + 1)
+      }
+    }
+  }
+  return dp[(1 << n) - 1]
+
+  function countBit(num) {
+    let count = 0
+    while (num > 0) {
+      num = num & (num - 1)
+      count++
+    }
+    return count
+  }
+}
+
+minNumberOfSemesters(
+  12,
+  [
+    [1, 2],
+    [1, 3],
+    [7, 5],
+    [7, 6],
+    [4, 8],
+    [8, 9],
+    [9, 10],
+    [10, 11],
+    [11, 12]
+  ],
+  2
+)
