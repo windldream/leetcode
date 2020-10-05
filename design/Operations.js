f5547cea896e6fbac1e11e14d2b4316f58deec64
“windldream” <1508353859@qq.com>
2020-10-05 16:56:07 +0800

    面试题 16.09. 运算

---
+++ design/Operations.js
@@ -0,0 +1,36 @@
+var Operations = function () {}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+Operations.prototype.minus = function (a, b) {
+  return a + -b
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+Operations.prototype.multiply = function (a, b) {
+  return a * b
+}
+
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+Operations.prototype.divide = function (a, b) {
+  return Math.trunc(a / b)
+}
+
+/**
+ * Your Operations object will be instantiated and called as such:
+ * var obj = new Operations()
+ * var param_1 = obj.minus(a,b)
+ * var param_2 = obj.multiply(a,b)
+ * var param_3 = obj.divide(a,b)
+ */
