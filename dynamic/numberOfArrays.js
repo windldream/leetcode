33879a38c47b6bc7394715d1e2b88edef9f35ff4
“windldream” <1508353859@qq.com>
2020-10-08 22:58:58 +0800

    1416. 恢复数组

---
+++ dynamic/numberOfArrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var numberOfArrays = function (s, k) {
+  const mod = 10 ** 9 + 7
+  const len = s.length
+  if (len < 1) return 1
+  const dp = Array(len + 1).fill(0)
+  dp[0] = 1
+  for (let i = 1; i <= len; i++) {
+    for (let j = i - 1; j >= 0 && i - j < 10; j--) {
+      const str = s.substring(j, i)
+      if (str[0] === '0') continue
+      const num = +str
+      if (num > k) break
+      dp[i] = (dp[i] + dp[j]) % mod
+    }
+  }
+  return dp[len]
+}
