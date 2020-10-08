5492a5cde028a0ed819d19ba0cc7ac7085320ff2
“windldream” <1508353859@qq.com>
2020-10-08 19:20:02 +0800

    1406. 石子游戏 III

--- dynamic/stoneGameIII.js
+++ dynamic/stoneGameIII.js
@@ -4,29 +4,21 @@
  */
 var stoneGameIII = function (stoneValue) {
   const n = stoneValue.length
-  const memo = Array(n)
-  const ans = dfs(stoneValue, 0)
-  if (ans > 0) {
-    return 'Alice'
-  } else if (ans < 0) {
-    return 'Bob'
+  const dp = Array(n + 3).fill(0)
+  let sum = 0
+  for (let i = n - 1; i >= 0; i--) {
+    dp[i] = -Infinity
+    sum += stoneValue[i]
+    for (let j = 1; j <= 3; j++) {
+      dp[i] = Math.max(dp[i], sum - dp[i + j])
+    }
   }
-  return 'Tie'
 
-  function dfs(stoneValue, start) {
-    const len = stoneValue.length
-    if (start === len) return 0
-    if (memo[start] !== undefined) return memo[start]
-    let ans = stoneValue[start] - dfs(stoneValue, start + 1)
-    if (start + 1 < len) {
-      ans = Math.max(ans, stoneValue[start] + stoneValue[start + 1] - dfs(stoneValue, start + 2))
-    }
-    if (start + 2 < len) {
-      ans = Math.max(
-        ans,
-        stoneValue[start] + stoneValue[start + 1] + stoneValue[start + 2] - dfs(stoneValue, start + 3)
-      )
-    }
-    return (memo[start] = ans)
+  if (dp[0] > sum - dp[0]) {
+    return 'Alice'
+  } else if (dp[0] < sum - dp[0]) {
+    return 'Bob'
+  } else {
+    return 'Tie'
   }
 }
