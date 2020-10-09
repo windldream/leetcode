9ba3311bb8f3fc6c85e79e55ea452a0141439c85
“windldream” <1508353859@qq.com>
2020-10-09 22:44:03 +0800

    1043. 分隔数组以得到最大和

--- dynamic/maxSumAfterPartitioning.js
+++ dynamic/maxSumAfterPartitioning.js
@@ -6,7 +6,6 @@
 var maxSumAfterPartitioning = function (arr, k) {
   const len = arr.length
   const dp = Array(len + 1).fill(0)
-  dp[0] = 0
   for (let i = 1; i <= len; i++) {
     for (let j = 1; j <= k; j++) {
       dp[i] = Math.max(dp[i], i >= j ? dp[i - j] + Math.max(...arr.slice(i - j, i)) * j : arr[i - 1])
