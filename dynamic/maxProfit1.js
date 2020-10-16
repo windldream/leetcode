e419c7befc8242a2fe91292bbe29e32704d8a9bc
“windldream” <1508353859@qq.com>
2020-10-16 21:02:29 +0800

    剑指 Offer 63. 股票的最大利润

--- dynamic/maxProfit1.js
+++ dynamic/maxProfit1.js
@@ -4,10 +4,14 @@
  */
 var maxProfit = function (prices) {
   const len = prices.length
+  if (len < 2) return 0
   let ans = 0
-  for (let i = 0; i < len; i++) {
-    for (let j = i + 1; j < len; j++) {
-      ans = Math.max(ans, prices[j] - prices[i])
+  let min = prices[0]
+  for (let i = 1; i < len; i++) {
+    if (prices[i] < min) {
+      min = prices[i]
+    } else {
+      ans = Math.max(ans, prices[i] - min)
     }
   }
   return ans
