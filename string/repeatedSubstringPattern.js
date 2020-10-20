f6b3029340499953f0248ec67840603cb3eb63c4
“windldream” <1508353859@qq.com>
2020-10-20 22:05:22 +0800

    459. 重复的子字符串

--- string/repeatedSubstringPattern.js
+++ string/repeatedSubstringPattern.js
@@ -3,12 +3,6 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function (s) {
-  const len = s.length
-  for (let i = 0; i < len >> 1; i++) {
-    const str = s.substring(0, i + 1)
-    if (len % str.length === 0) {
-      if (str.repeat(len / str.length) === s) return true
-    }
-  }
-  return false
+  const str = s + s
+  return str.substring(1, str.length - 1).includes(s)
 }
