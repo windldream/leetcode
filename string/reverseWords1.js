6c3ad28472bf8004eb99a33239218a2b95fb1c4a
“windldream” <1508353859@qq.com>
2020-10-21 20:27:33 +0800

    557. 反转字符串中的单词 III

---
+++ string/reverseWords1.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return s
+    .split(' ')
+    .map((str) => str.split('').reverse().join(''))
+    .join(' ')
+}
