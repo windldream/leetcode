29e570f08b3772fc174c193f6f1631604a0021bb
“windldream” <1508353859@qq.com>
2020-10-21 19:41:41 +0800

    551. 学生出勤记录 I

--- string/checkRecord.js
+++ string/checkRecord.js
@@ -3,19 +3,5 @@
  * @return {boolean}
  */
 var checkRecord = function (s) {
-  let a = 0
-  let l = 0
-  for (const c of s) {
-    if (c === 'A') {
-      if (a === 1) return false
-      a++
-      l = 0
-    } else if (c === 'L') {
-      if (l === 2) return false
-      l++
-    } else {
-      l = 0
-    }
-  }
-  return true
+  return s.indexOf('A') === s.lastIndexOf('A') && !s.includes('LLL')
 }
