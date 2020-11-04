72fa9b86f2fe06b94849f0743af14379c6c8fa62
“windldream” <1508353859@qq.com>
2020-11-04 23:17:09 +0800

    769. 最多能完成排序的块

--- array/maxChunksToSorted1.js
+++ array/maxChunksToSorted1.js
@@ -3,17 +3,27 @@
  * @return {number}
  */
 var maxChunksToSorted = function (arr) {
-  const stack = []
-  for (const num of arr) {
-    if (stack.length && num < stack[stack.length - 1]) {
-      const head = stack.pop()
-      while (stack.length && num < stack[stack.length - 1]) {
-        stack.pop()
-      }
-      stack.push(head)
-    } else {
-      stack.push(num)
+  const len = arr.length
+  const maxArr = []
+  const minArr = []
+
+  let max = -Infinity
+  for (let i = 0; i < len; i++) {
+    max = Math.max(max, arr[i])
+    maxArr[i] = max
+  }
+
+  let min = Infinity
+  for (let i = len - 1; i >= 0; i--) {
+    min = Math.min(min, arr[i])
+    minArr[i] = min
+  }
+
+  let ans = 0
+  for (let i = 0; i < len; i++) {
+    if (i === len - 1 || maxArr[i] < minArr[i + 1]) {
+      ans++
     }
   }
-  return stack.length
+  return ans
 }
