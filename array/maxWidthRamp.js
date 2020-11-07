61fc7def29ede678cb2d3fb71c72c74fd00dffa9
“windldream” <1508353859@qq.com>
2020-11-07 22:50:07 +0800

    962. 最大宽度坡

---
+++ array/maxWidthRamp.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var maxWidthRamp = function (A) {
+  const len = A.length
+  let ans = 0
+  for (let i = 0; i < len; i++) {
+    for (let j = len - 1; j > i; j--) {
+      if (A[j] >= A[i]) ans = Math.max(ans, j - i)
+    }
+  }
+  return ans
+}
