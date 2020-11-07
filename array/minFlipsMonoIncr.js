373cf615d53f3abdb153d4fb6e248f5de355421d
“windldream” <1508353859@qq.com>
2020-11-07 21:05:27 +0800

    926. 将字符串翻转到单调递增

---
+++ array/minFlipsMonoIncr.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} S
+ * @return {number}
+ */
+var minFlipsMonoIncr = function (S) {
+  const len = S.length
+  const left = Array(len).fill(0)
+  let total = 0
+  for (let i = 0; i < len; i++) {
+    total += S[i] === '1'
+    left[i] = total
+  }
+
+  const right = Array(len + 1).fill(0)
+  total = 0
+  for (let i = len - 1; i >= 0; i--) {
+    total += S[i] === '0'
+    right[i] = total
+  }
+
+  let ans = Infinity
+  for (let i = 0; i < len; i++) {
+    ans = Math.min(ans, left[i] + right[i + 1])
+  }
+  return Math.min(ans, len - left[len - 1])
+}
+
+minFlipsMonoIncr('11011')
