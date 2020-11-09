3ced3835812b2b711a08a3257ccd31e9a3e957bf
“windldream” <1508353859@qq.com>
2020-11-09 22:48:08 +0800

    1385. 两个数组间的距离值

---
+++ array/findTheDistanceValue.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+var findTheDistanceValue = function (arr1, arr2, d) {
+  let ans = 0
+  outer: for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (Math.abs(arr1[i] - arr2[j]) <= d) continue outer
+    }
+    ans++
+  }
+  return ans
+}
