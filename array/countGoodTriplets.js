513a6e5c8f84d9bbe891b3970904a29e16e3478b
“windldream” <1508353859@qq.com>
2020-11-10 23:06:06 +0800

    1534. 统计好三元组

---
+++ array/countGoodTriplets.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+var countGoodTriplets = function (arr, a, b, c) {
+  const len = arr.length
+  let ans = 0
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
+          ans++
+        }
+      }
+    }
+  }
+  return ans
+}
