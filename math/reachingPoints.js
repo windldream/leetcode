52e147ab9b943743b57371945456437c5a68d319
“windldream” <1508353859@qq.com>
2020-11-17 22:54:44 +0800

    780. 到达终点

--- math/reachingPoints.js
+++ math/reachingPoints.js
@@ -9,9 +9,9 @@ var reachingPoints = function (sx, sy, tx, ty) {
   while (tx > 0 && ty > 0) {
     if (sx === tx && sy === ty) return true
     if (tx > ty) {
-      tx -= ty
+      tx -= Math.max(Math.trunc((tx - sx) / ty), 1) * ty
     } else {
-      ty -= tx
+      ty -= Math.max(Math.trunc((ty - sy) / tx), 1) * tx
     }
   }
   return false
