34f5290bb88db65b00020c3bea4407a7b3960874
“windldream” <1508353859@qq.com>
2020-11-23 21:37:16 +0800

    剑指 Offer 49. 丑数

--- math/nthUglyNumber.js
+++ math/nthUglyNumber.js
@@ -6,22 +6,15 @@ var nthUglyNumber = function (n) {
   let a = 0
   let b = 0
   let c = 0
-  const dp = Array(n).fill(0)
-  dp[0] = 1
+  const ans = [1]
   for (let i = 1; i < n; i++) {
-    let num = Math.min(dp[a] * 2, dp[b] * 3, dp[c] * 5)
-    if (num === dp[a] * 2) {
-      a++
-    }
-    if (num === dp[b] * 3) {
-      b++
-    }
-    if (num === dp[c] * 5) {
-      c++
-    }
-    dp[i] = num
+    const min = Math.min(ans[a] * 2, ans[b] * 3, ans[c] * 5)
+    if (min === ans[a] * 2) a++
+    if (min === ans[b] * 3) b++
+    if (min === ans[c] * 5) c++
+    ans.push(min)
   }
-  return dp[n - 1]
+  return ans[n - 1]
 }
 
 nthUglyNumber(10)
