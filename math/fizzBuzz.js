009c84ba103dba9da3f31f15f9fdc53318e2c203
“windldream” <1508353859@qq.com>
2020-11-24 22:45:30 +0800

    412. Fizz Buzz

---
+++ math/fizzBuzz.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var fizzBuzz = function (n) {
+  const ans = []
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      ans.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      ans.push('Fizz')
+    } else if (i % 5 === 0) {
+      ans.push('Buzz')
+    } else {
+      ans.push(i + '')
+    }
+  }
+  return ans
+}
