baf15f76bc72d0910d69dacd233dd18cccbb342c
“windldream” <1508353859@qq.com>
2020-11-26 23:44:44 +0800

    1642. 可以到达的最远建筑

---
+++ stack/furthestBuilding.js
@@ -0,0 +1,88 @@
+class Heap {
+  constructor(compare = (a, b) => a - b > 0) {
+    this.heap = []
+    this.compare = compare
+  }
+
+  size() {
+    return this.heap.length
+  }
+
+  top() {
+    return this.heap[0]
+  }
+
+  insert(item) {
+    this.heap.push(item)
+    this.up(this.size() - 1)
+  }
+
+  remove() {
+    const delItem = this.heap[0]
+    this.swap(this.size() - 1, 0)
+    this.heap.length -= 1
+    this.down(0)
+    return delItem
+  }
+
+  down(k) {
+    let left = k * 2 + 1,
+      right = k * 2 + 2,
+      largest = k
+
+    if (left < this.size() && this.compare(this.heap[left], this.heap[largest])) {
+      largest = left
+    }
+
+    if (right < this.size() && this.compare(this.heap[right], this.heap[largest])) {
+      largest = right
+    }
+
+    if (largest !== k) {
+      this.swap(k, largest)
+      this.down(largest)
+    }
+  }
+
+  up(k) {
+    let parent = Math.floor((k - 1) / 2)
+    while (k > 0 && this.compare(this.heap[k], this.heap[parent])) {
+      this.swap(k, parent)
+      k = parent
+      parent = Math.floor((parent - 1) / 2)
+    }
+  }
+
+  swap(i, j) {
+    const tmp = this.heap[i]
+    this.heap[i] = this.heap[j]
+    this.heap[j] = tmp
+  }
+}
+
+/**
+ * @param {number[]} heights
+ * @param {number} bricks
+ * @param {number} ladders
+ * @return {number}
+ */
+var furthestBuilding = function (heights, bricks, ladders) {
+  const len = heights.length
+  if (len === 1) return 0
+
+  const pq = new Heap((a, b) => b - a > 0)
+  let i
+  for (i = 1; i < len; i++) {
+    const diff = heights[i] - heights[i - 1]
+    if (diff > 0) {
+      pq.insert(diff)
+      if (pq.size() > ladders) {
+        bricks -= pq.remove()
+      }
+      if (bricks < 0) break
+    }
+  }
+  return i - 1
+}
+
+furthestBuilding([4, 12, 2, 7, 3, 18, 20, 3, 19], 10, 2)
