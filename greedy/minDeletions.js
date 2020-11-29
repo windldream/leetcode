2b16fb54b1b06e4f6db1a4dca727b416c9dbf4b8
“windldream” <1508353859@qq.com>
2020-11-29 14:38:30 +0800

    1647. 字符频次唯一的最小删除次数

--- greedy/minDeletions.js
+++ greedy/minDeletions.js
@@ -2,4 +2,28 @@
  * @param {string} s
  * @return {number}
  */
-var minDeletions = function (s) {}
+var minDeletions = function (s) {
+  const map = new Map()
+  for (const c of s) {
+    if (!map.has(c)) {
+      map.set(c, 0)
+    }
+    map.set(c, map.get(c) + 1)
+  }
+
+  const values = [...map.values()].sort((a, b) => b - a)
+  let ans = 0
+  for (let i = 1; i < values.length; i++) {
+    if (values.indexOf(values[i]) !== values.lastIndexOf(values[i])) {
+      let val = values[i]
+      while (values.includes(val) && val !== 0) {
+        val -= 1
+        ans++
+      }
+      values[i] = val
+    }
+  }
+  return ans
+}
+
+minDeletions('bbcebab')
