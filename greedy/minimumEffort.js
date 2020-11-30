05c7bcf61b88279c426c61a2e982acb87e23fc5e
“windldream” <1508353859@qq.com>
2020-11-30 23:24:11 +0800

    1665. 完成所有任务的最少初始能量

---
+++ greedy/minimumEffort.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} tasks
+ * @return {number}
+ */
+var minimumEffort = function (tasks) {
+  tasks.sort((a, b) => a[0] - a[1] - (b[0] - b[1]))
+  let sum = 0
+  let ans = 0
+  for (const task of tasks) {
+    ans = Math.max(ans, sum + task[1])
+    sum += task[0]
+  }
+  return ans
+}
+
+minimumEffort([
+  [1, 1],
+  [1, 3]
+])
+
+// 6   ->   12 + 5 + 4 + 3 + 2 + 1
+// 12  ->
+
+// 4    ->  2
+// 12   ->
