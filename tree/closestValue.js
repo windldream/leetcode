3390de67cf95680a4fac3d0fd96bc9253386552b
“windldream” <1508353859@qq.com>
2020-12-06 14:16:54 +0800

    270. 最接近的二叉搜索树值

---
+++ tree/closestValue.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function (root, target) {
+  let ans = 0
+  let diff = Infinity
+  inorder(root)
+  return ans
+
+  function inorder(root) {
+    if (root === null) return
+    if (Math.abs(root.val - target) < diff) {
+      diff = Math.abs(root.val - target)
+      ans = root.val
+    }
+    inorder(root.left)
+    inorder(root.right)
+  }
+}
