02be33cef31e6de9b4f320c185023581b1bbd229
“windldream” <1508353859@qq.com>
2020-12-06 14:45:50 +0800

    285. 二叉搜索树中的顺序后继

---
+++ tree/inorderSuccessor2.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @return {TreeNode}
+ */
+var inorderSuccessor = function (root, p) {
+  if (root === null) return null
+  if (root.val <= p.val) return inorderSuccessor(root.right, p)
+  const left = inorderSuccessor(root.left, p)
+  if (left === null) return root
+  return left
+}
