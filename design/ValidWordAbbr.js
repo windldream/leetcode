f2e9905aed1accc3312d1ea5defe80ca9d1cee80
“windldream” <1508353859@qq.com>
2020-12-22 19:02:52 +0800

    288. 单词的唯一缩写

---
+++ design/ValidWordAbbr.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} dictionary
+ */
+const ValidWordAbbr = function(dictionary) {
+  const map = new Map()
+  for (const word of dictionary) {
+    const len = word.length
+    let str
+    if (len < 3) {
+      str = word
+    } else {
+      str = word[0] + (len - 2) + word[len - 1]
+    }
+    if (!map.has(str)) {
+      map.set(str, new Set())
+    }
+    map.get(str).add(word)
+  }
+  this.wordMap = map
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+ValidWordAbbr.prototype.isUnique = function(word) {
+  const len = word.length
+  let str
+  if (len < 3) {
+    str = word
+  } else {
+    str = word[0] + (len - 2) + word[len - 1]
+  }
+
+  if (!this.wordMap.has(str)) return true
+  if (this.wordMap.has(str)) {
+    const list = this.wordMap.get(str)
+    if (list.size === 1 && [...list.values()][0] === word) return true
+  }
+  return false
+};
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * var obj = new ValidWordAbbr(dictionary)
+ * var param_1 = obj.isUnique(word)
+ */
