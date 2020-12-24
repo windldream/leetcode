4fd7e1e65079df1df0de6d942f64f6ce761fcab7
“windldream” <1508353859@qq.com>
2020-12-24 21:29:04 +0800

    716. 最大栈

---
+++ design/MaxStack.js
@@ -0,0 +1,57 @@
+/**
+ * initialize your data structure here.
+ */
+const MaxStack = function() {
+  this.stack = []
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MaxStack.prototype.push = function(x) {
+  this.stack.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MaxStack.prototype.pop = function() {
+  return this.stack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MaxStack.prototype.top = function() {
+  if (this.stack.length) {
+    return this.stack[this.stack.length - 1]
+  }
+};
+
+/**
+ * @return {number}
+ */
+MaxStack.prototype.peekMax = function() {
+  return Math.max(...this.stack)
+};
+
+/**
+ * @return {number}
+ */
+MaxStack.prototype.popMax = function() {
+  const max = Math.max(...this.stack)
+  const index = this.stack.lastIndexOf(max)
+  this.stack.splice(index, 1)
+  return max
+};
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * var obj = new MaxStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.peekMax()
+ * var param_5 = obj.popMax()
+ */
