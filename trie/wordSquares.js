bd6b98ce698c7ada317d60f650e32112fbe9f48b
“windldream” <1508353859@qq.com>
2020-12-26 21:21:11 +0800

    425. 单词方块

---
+++ trie/wordSquares.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} words
+ * @return {string[][]}
+ */
+const wordSquares = function(words) {
+  const ans = []
+  const prefixHashTable = buildPrefixHashTable(words)
+  const n = words[0].length
+  for (const word of words) {
+    const wordSquares = []
+    wordSquares.push(word)
+    backtracking(1, wordSquares)
+  }
+  return ans
+
+  function backtracking(step, wordSquares) {
+    if (step === n) {
+      ans.push(wordSquares.slice())
+      return
+    }
+    let prefix = ''
+    for (const word of wordSquares) {
+      prefix += word[step]
+    }
+    const candidates = prefixHashTable.has(prefix) ? prefixHashTable.get(prefix) : []
+    for (const candidate of candidates) {
+      wordSquares.push(candidate)
+      backtracking(step + 1, wordSquares)
+      wordSquares.pop()
+    }
+  }
+
+  function buildPrefixHashTable(words) {
+    const prefixHashTable = new Map()
+    const len = words[0].length
+    for (const word of words) {
+      for (let i = 1; i < len; i++) {
+        const prefix = word.substring(0, i)
+        if (!prefixHashTable.has(prefix)) {
+          prefixHashTable.set(prefix, new Set())
+        }
+        prefixHashTable.get(prefix).add(word)
+      }
+    }
+    return prefixHashTable
+  }
+};
