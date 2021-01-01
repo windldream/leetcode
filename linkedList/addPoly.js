35d570d727a5b3a2d0964c7c57929fc1992b851d
“windldream” <1508353859@qq.com>
2021-01-01 09:53:31 +0800

    1634. 求两个多项式链表的和

--- linkedList/addPoly.js
+++ linkedList/addPoly.js
@@ -17,11 +17,10 @@ var addPoly = function(poly1, poly2) {
   let cur = dummy
   while (poly1 !== null || poly2 !== null) {
     if (poly1 === null) {
-      cur.next = new PolyNode(poly2.coefficient, poly2.power)
-      poly2 = poly2.next
+      cur.next = poly2
+      break
     } else if (poly2 === null) {
-      cur.next = new PolyNode(poly1.coefficient, poly1.power)
-      poly1 = poly1.next
+      cur.next = poly1
     } else {
       if (poly1.power > poly2.power) {
         cur.next = new PolyNode(poly1.coefficient, poly1.power)
