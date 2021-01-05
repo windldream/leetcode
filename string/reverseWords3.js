840dd5162e11d577633e92761b960993db876313
“windldream” <1508353859@qq.com>
2021-01-05 19:27:30 +0800

    186. 翻转字符串里的单词 II

--- string/reverseWords3.js
+++ string/reverseWords3.js
@@ -3,8 +3,22 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 const reverseWords = function(s) {
-  const strs = s.join('').split(' ').reverse().join(' ')
-  for (let i = 0; i < strs.length; i++) {
-    s[i] = strs[i]
+  const n = s.length
+  let start = 0
+  for (let i = 0; i < n; i++) {
+    if (s[i] === ' ') {
+      reverse(s, start, i - 1)
+      start = i + 1
+    }
+  }
+  reverse(s, start, n - 1)
+  reverse(s, 0, n - 1)
+
+  function reverse(s, start, end) {
+    while (start < end) {
+      [s[start], s[end]] = [s[end], s[start]]
+      start++
+      end--
+    }
   }
 };
