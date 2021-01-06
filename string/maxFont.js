8cf38ecaab1d52402a60e651d067765f9d4128a4
“windldream” <1508353859@qq.com>
2021-01-06 23:28:39 +0800

    1618. 找出适应屏幕的最大字号

---
+++ string/maxFont.js
@@ -0,0 +1,56 @@
+/**
+ * // This is the FontInfo's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function FontInfo() {
+ *
+ *		@param {number} fontSize
+ *		@param {char} ch
+ *     	@return {number}
+ *     	this.getWidth = function(fontSize, ch) {
+ *      	...
+ *     	};
+ *
+ *		@param {number} fontSize
+ *     	@return {number}
+ *     	this.getHeight = function(fontSize) {
+ *      	...
+ *     	};
+ * };
+ */
+/**
+ * @param {string} text
+ * @param {number} w
+ * @param {number} h
+ * @param {number[]} fonts
+ * @param {FontInfo} fontInfo
+ * @return {number}
+ */
+const maxFont = function(text, w, h, fonts, fontInfo) {
+  let lo = 0
+  let hi = fonts.length - 1
+  while (lo <= hi) {
+    const mid = (lo + hi) >> 1
+    const [width, height] = getWidthAndHeight(text, fontInfo, fonts[mid])
+    if (width > w || height > h) {
+      hi = mid - 1
+    } else if (width < w || height < h) {
+      lo = mid + 1
+    } else {
+      return fonts[mid]
+    }
+  }
+
+  const [width, height] = getWidthAndHeight(text, fontInfo, fonts[lo])
+  if (width > w || height > h || lo > hi) lo--
+  if (lo === -1) return -1
+  return fonts[lo]
+
+  function getWidthAndHeight(text, fontInfo, fontSize) {
+    let w = 0
+    let h = fontInfo.getHeight(fontSize)
+    for (const str of text) {
+      w += fontInfo.getWidth(fontSize, str)
+    }
+    return [w, h]
+  }
+};
