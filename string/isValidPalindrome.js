19343d29c14eec39569c1993813adafa93941846
“windldream” <1508353859@qq.com>
2021-01-06 23:05:29 +0800

    1216. 验证回文字符串 III

--- string/isValidPalindrome.js
+++ string/isValidPalindrome.js
@@ -8,7 +8,6 @@ const isValidPalindrome = function(s, k) {
   const dp = Array(len).fill(0).map(() => Array(len).fill(0))
   for (let i = 0; i < len; i++) dp[i][i] = 1
 
-  let max = 1
   for (let i = len - 1; i >= 0; i--) {
     for (let j = i + 1; j < len; j++) {
       if (s[i] === s[j]) {
@@ -16,12 +15,11 @@ const isValidPalindrome = function(s, k) {
       } else {
         dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
       }
-      max = Math.max(max, dp[i][j])
     }
   }
 
-  return max >= len - k
+  return dp[0][len - 1] >= len - k
 };
 
-// dp[i][j] 表示以 i 开始 以 j 结尾的最大回文字串
-// dp[i][j] === dp[i + 1][j - 1] + 1
+// dp[i][j] 表示以 i 开始 以 j 结尾的最大回文字串长度
+// dp[i][j] === dp[i + 1][j - 1] + 2
