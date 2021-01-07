2bc85eb4d67cbf5e095dc4d77379d46c64d3f56f
“windldream” <1508353859@qq.com>
2021-01-07 17:57:58 +0800

    540. 有序数组中的单一元素

--- binarySearch/singleNonDuplicate.js
+++ binarySearch/singleNonDuplicate.js
@@ -2,10 +2,29 @@
  * @param {number[]} nums
  * @return {number}
  */
-var singleNonDuplicate = function(nums) {
-  let ans = 0
-  for (const num of nums) {
-    ans = ans ^ num
+var singleNonDuplicate = function (nums) {
+  const len = nums.length
+  let lo = 0
+  let hi = len - 1
+  while (lo <= hi) {
+    const mid = (lo + hi) >> 1
+    if (nums[mid] === nums[mid - 1]) {
+      if ((mid - lo) % 2 === 0) {
+        hi = mid - 2
+      } else {
+        lo = mid + 1
+      }
+    } else if (nums[mid] === nums[mid + 1]) {
+      if ((hi - mid) % 2 === 0) {
+        lo = mid + 2
+      } else {
+        hi = mid - 1
+      }
+    } else {
+      return nums[mid]
+    }
   }
-  return ans
-};
+  return nums[hi]
+}
+
+singleNonDuplicate([1, 1, 2, 2, 3])
