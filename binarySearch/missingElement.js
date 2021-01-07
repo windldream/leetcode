83f318ab31fbc740979ca917eebdcfd187b1df42
“windldream” <1508353859@qq.com>
2021-01-07 22:02:33 +0800

    1060. 有序数组中的缺失元素

--- binarySearch/missingElement.js
+++ binarySearch/missingElement.js
@@ -4,22 +4,17 @@
  * @return {number}
  */
 const missingElement = function (nums, k) {
-  let lo = 0
-  let hi = nums.length
-  while (lo < hi) {
-    const mid = (lo + hi) >> 1
-    if (getCount(nums, mid) < k) {
-      lo = mid + 1
+  const len = nums.length
+  for (let i = 0; i < len - 1; i++) {
+    const loss = nums[i + 1] - nums[i] - 1
+    if (loss >= k) {
+      return nums[i] + k
     } else {
-      hi = mid
+      k -= loss
     }
   }
 
-  return nums[lo - 1] + k - getCount(nums, lo - 1)
-
-  function getCount(nums, end) {
-    return nums[end] - nums[0] - end
-  }
+  return nums[len - 1] + k
 }
 
 missingElement([89248, 1001459, 1923894, 2312461, 4064902, 4502493, 8228616, 8262691, 8654597, 9635457], 10)
