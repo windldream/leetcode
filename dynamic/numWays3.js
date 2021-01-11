a1ee50ee8115e6332504b6997a9c7774c08ee7d9
“windldream” <1508353859@qq.com>
2021-01-11 21:22:58 +0800

    1639. 通过给定词典构造目标字符串的方案数

---
+++ dynamic/numWays3.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string[]} words
+ * @param {string} target
+ * @return {number}
+ */
+const numWays = function (words, target) {
+  const mod = 10 ** 9 + 7
+  const n = words[0].length
+  const m = target.length
+  const maps = Array(n)
+    .fill(0)
+    .map(() => Array(26).fill(0))
+  for (let i = 0; i < n; i++) {
+    for (const word of words) {
+      const index = getIndex(word[i], 'a')
+      maps[i][index] += 1
+    }
+  }
+
+  const dp = Array(n)
+    .fill(0)
+    .map(() => Array(m).fill(0))
+  const firstIndex = getIndex(target[0], 'a')
+  dp[0][0] = maps[0][firstIndex]
+  for (let i = 1; i < n; i++) {
+    dp[i][0] = (dp[i - 1][0] + maps[i][firstIndex]) % mod
+    for (let j = 1; j < m; j++) {
+      const count = maps[i][getIndex(target[j], 'a')]
+      dp[i][j] = (dp[i - 1][j] + ((count * dp[i - 1][j - 1]) % mod)) % mod
+    }
+  }
+  return dp[n - 1][m - 1]
+
+  function getIndex(a, b) {
+    return a.charCodeAt() - b.charCodeAt()
+  }
+}
+
+numWays(['acca', 'bbbb', 'caca'], 'aba')
