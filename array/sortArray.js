7baae49aeb647378dc59b2a471bb79652f2ef4f8
“windldream” <1508353859@qq.com>
2021-01-23 21:30:48 +0800

    912. 排序数组（选择排序）

--- array/sortArray.js
+++ array/sortArray.js
@@ -3,18 +3,20 @@
  * @return {number[]}
  */
 var sortArray = function (nums) {
-  return bubbleSort(nums)
+  return selectionSort(nums)
 
-  function bubbleSort(arr) {
+  function selectionSort(arr) {
     const len = arr.length
     for (let i = 0; i < len - 1; i++) {
-      for (let j = 0; j < len - 1 - i; j++) {
-        if (arr[j] > arr[j + 1]) {
-          const temp = arr[j]
-          arr[j] = arr[j + 1]
-          arr[j + 1] = temp
+      let minIndex = i
+      for (let j = i + 1; j < len; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j
         }
       }
+      const temp = arr[i]
+      arr[i] = arr[minIndex]
+      arr[minIndex] = temp
     }
     return arr
   }
