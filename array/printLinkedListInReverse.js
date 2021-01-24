66f1c0f9022a47b93a6f09a341fe50c60aa2a4ca
“windldream” <1508353859@qq.com>
2021-01-25 00:18:34 +0800

    1265. 逆序打印不可变链表

---
+++ array/printLinkedListInReverse.js
@@ -0,0 +1,25 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    };
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function (head) {
+  if (head === null) return
+  printLinkedListInReverse(head.getNext())
+  head.printValue()
+}
