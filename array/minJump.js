f8494819e6313a651ae973eee9e8aebfa8217b77
“windldream” <1508353859@qq.com>
2021-01-29 12:41:42 +0800

    LCP 09. 最小跳跃次数

--- array/minJump.js
+++ array/minJump.js
@@ -4,24 +4,15 @@
  */
 var minJump = function (jump) {
   const n = jump.length
-  const dp = Array(n + 1).fill(Infinity)
-  dp[0] = 0
-  for (let i = 0; i < n; i++) {
-    if (i + jump[i] >= n) {
-      dp[n] = Math.min(dp[n], dp[i] + 1)
-    } else {
-      dp[i + jump[i]] = Math.min(dp[i + jump[i]], dp[i] + 1)
-    }
-    for (let j = 1; j < i; j++) {
-      dp[j] = Math.min(dp[j], dp[i] + 1)
-      if (j + jump[j] >= n) {
-        dp[n] = Math.min(dp[n], dp[j] + 1)
-      } else {
-        dp[j + jump[j]] = Math.min(dp[j + jump[j]], dp[j] + 1)
-      }
+  const dp = Array(n).fill(0)
+  dp[n - 1] = 1
+  for (let i = n - 2; i >= 0; i--) {
+    dp[i] = i + jump[i] >= n ? 1 : dp[i + jump[i]] + 1
+    for (let j = i + 1; j < n && dp[j] >= dp[i] + 1; j++) {
+      dp[j] = dp[i] + 1
     }
   }
-  return dp[n]
+  return dp[0]
 }
 
 minJump([3, 7, 6, 1, 4, 3, 7, 8, 1, 2, 8, 5, 9, 8, 3, 2, 7, 5, 1, 1])
