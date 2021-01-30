829a00789feb87f1a7581566f3fa9169f0aa69ef
“windldream” <1508353859@qq.com>
2021-01-30 22:21:43 +0800

    LCP 19. 秋叶收藏集

--- array/minimumOperations.js
+++ array/minimumOperations.js
@@ -8,22 +8,24 @@ var minimumOperations = function (leaves) {
     .fill(0)
     .map(() => Array(n).fill(0))
   for (let i = 0; i < n; i++) {
+    const red = leaves[i] === 'r' ? 0 : 1
+    const yellow = leaves[i] === 'y' ? 0 : 1
     if (i < 1) {
-      dp[0][i] = leaves[i] === 'r' ? 0 : 1
+      dp[0][i] = red
     } else {
-      dp[0][i] = dp[0][i - 1] + (leaves[i] === 'r' ? 0 : 1)
+      dp[0][i] = dp[0][i - 1] + red
     }
 
     if (i < 1) {
       dp[1][i] = dp[0][i]
     } else {
-      dp[1][i] = Math.min(dp[0][i - 1] + (leaves[i] === 'y' ? 0 : 1), dp[1][i - 1] + (leaves[i] === 'y' ? 0 : 1))
+      dp[1][i] = Math.min(dp[0][i - 1] + yellow, dp[1][i - 1] + yellow)
     }
 
     if (i < 2) {
       dp[2][i] = dp[1][i]
     } else {
-      dp[2][i] = Math.min(dp[1][i - 1] + (leaves[i] === 'r' ? 0 : 1), dp[2][i - 1] + (leaves[i] === 'r' ? 0 : 1))
+      dp[2][i] = Math.min(dp[1][i - 1] + red, dp[2][i - 1] + red)
     }
   }
   return dp[2][n - 1]
