85a36c5341fe7e9758ad820dc4965baf8ff35fcd
“windldream” <1508353859@qq.com>
2021-02-02 10:08:42 +0800

    580. 统计各专业学生人数

--- sql/student.sql
+++ sql/student.sql
@@ -2,5 +2,5 @@ select dept_name, count(student_id) as student_number
 from department
   left join student
   on department.dept_id = student.dept_id
-group by department.dept_name
-order by student_number desc, department.dept_name
+group by dept_name
+order by student_number desc, dept_name
