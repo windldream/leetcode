00781abced5e08a519f42736de6630cd88a158fd
“windldream” <1508353859@qq.com>
2021-02-02 19:50:34 +0800

    612. 平面上的最近距离

---
+++ sql/point_2d.sql
@@ -0,0 +1,12 @@
+select round
+  (
+    sqrt(
+      min(
+        pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2)
+      )
+    ),
+    2
+  ) as shortest
+from point_2d p1
+join point_2d p2
+on p1.x != p2.x or p1.y != p2.y
