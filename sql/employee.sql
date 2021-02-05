64f19ce24eeb5be0b1b0c854716840204e05fe2c
“windldream” <1508353859@qq.com>
2021-02-05 09:42:42 +0800

    1270. 向公司CEO汇报工作的所有人

---
+++ sql/employee.sql
@@ -0,0 +1,6 @@
+select e1.employee_id
+from Employees e1, Employees e2, Employees e3
+where e1.manager_id = e2.employee_id 
+    and e2.manager_id = e3.employee_id
+    and e3.manager_id = 1
+    and e1.employee_id != 1
