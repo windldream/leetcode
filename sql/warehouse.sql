28c62509f2ebf8f1728c66e67dab269be66ee901
“windldream” <1508353859@qq.com>
2021-02-09 10:36:33 +0800

    1571. 仓库经理

--- sql/warehouse.sql
+++ sql/warehouse.sql
@@ -2,6 +2,6 @@ select
   name as WAREHOUSE_NAME,
   sum(w.units * p.Width * p.Length * p.Height) as VOLUME
 from Warehouse w
-left join Products p
+join Products p
 on w.product_id = p.product_id
 group by WAREHOUSE_NAME
