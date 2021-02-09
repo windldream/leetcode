e0015c7b7e4c46f435bef436c438c590a6e15f90
“windldream” <1508353859@qq.com>
2021-02-09 16:23:23 +0800

    1623. 三人国家代表队

---
+++ sql/school.sql
@@ -0,0 +1,11 @@
+select
+  a.student_name as member_A,
+  b.student_name as member_B,
+  c.student_name as member_C,
+from SchoolA a, SchoolB b, SchoolC c
+where a.student_name != b.student_name
+  and b.student_name != c.student_name
+  and a.student_name != c.student_name
+  and a.student_id != b.student_id
+  and b.student_id != c.student_id
+  and a.student_id != c.student_id
