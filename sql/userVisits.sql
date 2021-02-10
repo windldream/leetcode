7c88fc625039119ab7fc595815131850af93e89b
“windldream” <1508353859@qq.com>
2021-02-10 08:14:30 +0800

    1709. 访问日期之间最大的空档期

---
+++ sql/userVisits.sql
@@ -0,0 +1,11 @@
+select user_id, max(diff) as biggest_window
+from (
+  select 
+    user_id,
+    datediff(
+      lead(visit_date, 1, '2021-01-01') over(partition by user_id order by visit_date),
+      visit_date
+    ) as diff
+  from UserVisits
+) tmp
+group by user_id
