2ebdfdee820edecdfa68fa7d186137fe5f9f8f40
“windldream” <1508353859@qq.com>
2021-02-10 10:38:31 +0800

    194. 转置文件

---
+++ bash/file.sh
@@ -0,0 +1,15 @@
+# Read from the file file.txt and print its transposed content to stdout.
+awk '{
+  for (i=1; i<=NF; i++) {
+    if(NR==1){
+      res[i]=$i
+    } else {
+      res[i]=res[i] " " $i
+    }
+  }
+}
+END {
+  for (i=1; i<=NF; i++) {
+    print res[i]
+  }
+}' file.txt
