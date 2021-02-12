8d6a786b1dd4ce65cf24cb65b3a35853eb62e0eb
“windldream” <1508353859@qq.com>
2021-02-13 07:32:51 +0800

    448. 找到所有数组中消失的数字(抽屉原理)

---
+++ daily/findDisappearedNumbers.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  const n = nums.length
+  for (let i = 0; i < n; i++) {
+    while (nums[nums[i] - 1] !== nums[i]) {
+      ;[nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
+    }
+  }
+
+  const ans = []
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i + 1) {
+      ans.push(i + 1)
+    }
+  }
+  return ans
+}
+
+// [4,3,2,7,8,2,3,1]
+// [7,3,2,4,8,2,3,1]
+// [3,3,2,4,8,2,7,1]
+// [2,3,3,4,8,2,7,1]
+// [3,2,3,4,8,2,7,1]
+// [3,2,3,4,1,2,7,8]
+// [1,2,3,4,3,2,7,8]
