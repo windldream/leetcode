301268e29e3a8f60af8ce9196618d17074ec3c35
“windldream” <1508353859@qq.com>
2021-02-13 07:40:01 +0800

    448. 找到所有数组中消失的数字

--- daily/findDisappearedNumbers.js
+++ daily/findDisappearedNumbers.js
@@ -2,17 +2,34 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// var findDisappearedNumbers = function (nums) {
+//   const n = nums.length
+//   for (let i = 0; i < n; i++) {
+//     while (nums[nums[i] - 1] !== nums[i]) {
+//       ;[nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
+//     }
+//   }
+
+//   const ans = []
+//   for (let i = 0; i < n; i++) {
+//     if (nums[i] !== i + 1) {
+//       ans.push(i + 1)
+//     }
+//   }
+//   return ans
+// }
+
 var findDisappearedNumbers = function (nums) {
   const n = nums.length
+  // 找不到的数都不会加n
   for (let i = 0; i < n; i++) {
-    while (nums[nums[i] - 1] !== nums[i]) {
-      ;[nums[nums[i] - 1], nums[i]] = [nums[i], nums[nums[i] - 1]]
-    }
+    const index = (nums[i] - 1) % n
+    nums[index] += n
   }
 
   const ans = []
   for (let i = 0; i < n; i++) {
-    if (nums[i] !== i + 1) {
+    if (nums[i] <= n) {
       ans.push(i + 1)
     }
   }
