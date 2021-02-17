52feb5d239cf2852340dbbed7964f5d629472712
“windldream” <1508353859@qq.com>
2021-02-17 08:00:31 +0800

    19. 删除链表的倒数第 N 个结点

---
+++ second/removeNthFromEnd.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let fast = head
+  while (n > 0) {
+    fast = fast.next
+    n--
+  }
+  if (fast === null) return head.next
+
+  let slow = head
+  while (fast && fast.next) {
+    fast = fast.next
+    slow = slow.next
+  }
+  if (fast === null) if (slow.next === null) return slow.next
+  slow.next = slow.next.next
+  return head
+}
