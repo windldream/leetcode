fb008343cb72cdb7ad931edf3d5194a0483223b9
“windldream” <1508353859@qq.com>
2021-02-18 09:04:54 +0800

    22. 括号生成

--- second/generateParenthesis.js
+++ second/generateParenthesis.js
@@ -2,38 +2,57 @@
  * @param {number} n
  * @return {string[]}
  */
-var generateParenthesis = function (n) {
-  const ans = new Set()
-  const visited = new Set()
-  backtracking(n - 1, '()')
-  return [...ans]
+// var generateParenthesis = function (n) {
+//   const ans = new Set()
+//   const visited = new Set()
+//   backtracking(n - 1, '()')
+//   return [...ans]
 
-  function backtracking(n, str) {
-    if (!check(str)) return
-    if (n === 0) {
-      ans.add(str)
+//   function backtracking(n, str) {
+//     if (!check(str)) return
+//     if (n === 0) {
+//       ans.add(str)
+//       return
+//     }
+//     if (visited.has(str)) return
+//     for (let i = 0; i < str.length; i++) {
+//       const s = str.substring(0, i) + '(' + str.substring(i)
+//       for (let j = 0; j < s.length; j++) {
+//         backtracking(n - 1, s.substring(0, j) + ')' + s.substring(j))
+//       }
+//     }
+//     visited.add(str)
+//   }
+
+//   function check(str) {
+//     const stack = []
+//     for (let i = 0; i < str.length; i++) {
+//       if (str[i] === '(') {
+//         stack.push(str[i])
+//       } else {
+//         stack.pop()
+//       }
+//     }
+//     return stack.length === 0
+//   }
+// }
+
+var generateParenthesis = function (n) {
+  const ans = []
+  backtracking(n, n, '')
+  return ans
+
+  function backtracking(l, r, str) {
+    if (l === 0 && r === 0) {
+      ans.push(str)
       return
     }
-    if (visited.has(str)) return
-    for (let i = 0; i < str.length; i++) {
-      const s = str.substring(0, i) + '(' + str.substring(i)
-      for (let j = 0; j < s.length; j++) {
-        backtracking(n - 1, s.substring(0, j) + ')' + s.substring(j))
-      }
+    if (l > 0) {
+      backtracking(l - 1, r, str + '(')
     }
-    visited.add(str)
-  }
-
-  function check(str) {
-    const stack = []
-    for (let i = 0; i < str.length; i++) {
-      if (str[i] === '(') {
-        stack.push(str[i])
-      } else {
-        stack.pop()
-      }
+    if (l < r) {
+      backtracking(l, r - 1, str + ')')
     }
-    return stack.length === 0
   }
 }
 
