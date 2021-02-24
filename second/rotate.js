855ef709e43aba90a69e1430bd200ed7e2208211
“windldream” <1508353859@qq.com>
2021-02-24 16:54:15 +0800

    https://leetcode-cn.com/problems/rotate-image/

---
+++ second/rotate.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  const n = matrix.length
+  for (let i = 0; i < n >> 1; i++) {
+    for (let j = i; j < n - i - 1; j++) {
+      let temp = matrix[i][j]
+      matrix[i][j] = matrix[n - j - 1][i]
+      matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+      matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+      matrix[j][n - i - 1] = temp
+    }
+  }
+}
+
+//
+
+// 0 0 => 0 2
+// 0 1 => 1 2
+// 0 2 => 2 0
