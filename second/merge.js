b0ac89562cabc5eb12cac4127650a3d3565b0073
“windldream” <1508353859@qq.com>
2021-02-25 15:48:03 +0800

    56. 合并区间

--- second/merge.js
+++ second/merge.js
@@ -3,36 +3,17 @@
  * @return {number[][]}
  */
 var merge = function (intervals) {
-  intervals.sort((a, b) => a[1] - b[1])
+  intervals.sort((a, b) => a[0] - b[0])
+  const n = intervals.length
   const ans = []
-  for (let [start, end] of intervals) {
-    if (ans.length === 0) {
-      ans.push([start, end])
-    } else {
-      let [s, e] = ans.pop()
-      if (start <= s) {
-        if (ans.length && ans[ans.length - 1][1] >= start) {
-          while (ans.length && ans[ans.length - 1][1] >= start) {
-            ;[s, e] = ans.pop()
-          }
-          if (start <= s) {
-            ans.push([start, end])
-          } else {
-            ans.push([s, end])
-          }
-        } else {
-          ans.push([start, end])
-        }
-      } else {
-        // start > s
-        if (start <= e) {
-          ans.push([s, end])
-        } else {
-          ans.push([s, e])
-          ans.push([start, end])
-        }
-      }
+  let i = 0
+  while (i < n) {
+    let [start, end] = intervals[i]
+    while (i < n - 1 && intervals[i + 1][0] <= end) {
+      end = Math.max(intervals[++i][1], end)
     }
+    ans.push([start, end])
+    i++
   }
 
   return ans
