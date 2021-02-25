fb8d0b6a28197de32bb0d703bfa455bd26571078
“windldream” <1508353859@qq.com>
2021-02-25 15:03:02 +0800

    55. 跳跃游戏

---
+++ second/canJump.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function (nums) {
+  let k = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (i > k) return false
+    k = Math.max(k, i + nums[i])
+  }
+  return true
+}
+
+canJump([2, 3, 1, 1, 4])
