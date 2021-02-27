46bb76c473046157c959ececbbc748e5d0abfd57
“windldream” <1508353859@qq.com>
2021-02-27 08:09:28 +0800

    84. 柱状图中最大的矩形

--- top100/largestRectangleArea.js
+++ top100/largestRectangleArea.js
@@ -2,4 +2,20 @@
  * @param {number[]} heights
  * @return {number}
  */
-var largestRectangleArea = function (heights) {}
+var largestRectangleArea = function (heights) {
+  heights.unshift(0)
+  heights.push(0)
+  const n = heights.length
+  const stack = []
+  let ans = 0
+  for (let i = 0; i < n; i++) {
+    while (stack.length && heights[stack[stack.length - 1]] > heights[i]) {
+      const index = stack.pop()
+      ans = Math.max(ans, (i - 1 - stack[stack.length - 1]) * heights[index])
+    }
+    stack.push(i)
+  }
+  return ans
+}
+
+largestRectangleArea([2, 1, 3, 4, 2, 3])
