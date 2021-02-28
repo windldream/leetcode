6eafe0e6bc54951e62c1a0ee4318c15c524dea6d
“windldream” <1508353859@qq.com>
2021-02-28 21:28:22 +0800

    101. 对称二叉树

--- top100/isSymmetric.js
+++ top100/isSymmetric.js
@@ -10,13 +10,34 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
+// var isSymmetric = function (root) {
+//   if (root === null) return true
+//   return dfs(root.left, root.right)
+//   function dfs(left, right) {
+//     if (left === null && right === null) return true
+//     if (left === null || right === null) return false
+//     if (left.val !== right.val) return false
+//     return dfs(left.left, right.right) && dfs(left.right, right.left)
+//   }
+// }
+
 var isSymmetric = function (root) {
   if (root === null) return true
-  return dfs(root.left, root.right)
-  function dfs(left, right) {
-    if (left === null && right === null) return true
-    if (left === null || right === null) return false
-    if (left.val !== right.val) return false
-    return dfs(left.left, right.right) && dfs(left.right, right.left)
+  let stack = []
+  stack.push(root)
+  while (stack.length) {
+    const len = stack.length
+    const temp = []
+    for (let i = 0; i < len; i++) {
+      const head = stack[i]
+      const tail = stack[len - 1 - i]
+      if (head === null && tail === null) continue
+      if (head === null || tail === null) return false
+      if (head.val !== tail.val) return false
+      temp.push(head.left)
+      temp.push(head.right)
+    }
+    stack = temp
   }
+  return true
 }
