020ec63a922a39414788446c85217fd74e21b8e0
“windldream” <1508353859@qq.com>
2021-02-28 13:26:06 +0800

    5690. 最接近目标价格的甜点成本

---
+++ greedy/closestCost.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} baseCosts
+ * @param {number[]} toppingCosts
+ * @param {number} target
+ * @return {number}
+ */
+var closestCost = function (baseCosts, toppingCosts, target) {
+  const n = baseCosts.length
+  let ans = Infinity
+  for (let i = 0; i < n; i++) {
+    let cost = baseCosts[i]
+    dfs(cost, 0, toppingCosts, target)
+  }
+  return ans
+
+  function dfs(cost, index, costs, target) {
+    if (index === costs.length * 2) {
+      if (Math.abs(cost - target) < Math.abs(ans - target)) {
+        ans = cost
+      } else if (Math.abs(cost - target) === Math.abs(ans - target) && cost < ans) {
+        ans = cost
+      }
+      return
+    }
+    if (cost - target >= 0 && Math.abs(cost - target) > Math.abs(ans - target)) return
+    dfs(cost, index + 1, costs, target)
+    dfs(cost + costs[index % costs.length], index + 1, costs, target)
+  }
+}
+
+closestCost([1, 7], [3, 4], 10)
