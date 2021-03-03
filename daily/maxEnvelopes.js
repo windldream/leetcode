48590cf814a3d7459a5dbd0301edfbec742136fc
“windldream” <1508353859@qq.com>
2021-03-04 07:19:38 +0800

    354. 俄罗斯套娃信封问题

---
+++ daily/maxEnvelopes.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} envelopes
+ * @return {number}
+ */
+// var maxEnvelopes = function (envelopes) {
+//   envelopes.sort((a, b) => a[0] - b[0])
+//   const n = envelopes.length
+//   if (n === 0) return 0
+
+//   const dp = Array(n).fill(0)
+//   let ans = 0
+//   for (let i = 0; i < n; i++) {
+//     dp[i] = 1
+//     for (let j = 0; j < i; j++) {
+//       if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+//         dp[i] = Math.max(dp[i], dp[j] + 1)
+//       }
+//     }
+//     ans = Math.max(ans, dp[i])
+//   }
+//   return ans
+// }
+
+maxEnvelopes([
+  [5, 4],
+  [6, 4],
+  [6, 7],
+  [2, 3]
+])
