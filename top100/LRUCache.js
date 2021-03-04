da5484deb2f397455bfa39b3415c16fb9192f0c9
“windldream” <1508353859@qq.com>
2021-03-04 20:59:58 +0800

    146. LRU 缓存机制

--- top100/LRUCache.js
+++ top100/LRUCache.js
@@ -1,10 +1,61 @@
+class Node {
+  constructor(key, val) {
+    this.key = key
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoubleList {
+  constructor(head, tail) {
+    this.head = null
+    this.tail = null
+  }
+
+  addFirst(node) {
+    if (this.head === null) {
+      this.head = node
+      this.tail = node
+    } else {
+      const head = this.head
+      // 将当前节点的prev指针指向node
+      head.prev = node
+      // 将node的prev指针指向当前节点
+      node.next = head
+      this.head = node
+    }
+  }
+
+  remove(node) {
+    if (this.head === node && this.tail === node) {
+      this.head = null
+      this.tail = null
+    } else if (this.head === node) {
+      node.next.prev = null
+      this.head = node.next
+    } else if (this.tail === node) {
+      node.prev.next = null
+      this.tail = node.prev
+    } else {
+      node.prev.next = node.next
+      node.next.prev = node.prev
+    }
+  }
+
+  removeLast() {
+    const node = this.tail
+    this.remove(this.tail)
+    return node
+  }
+}
 /**
  * @param {number} capacity
  */
 var LRUCache = function (capacity) {
-  this.stack = []
   this.cache = new Map()
-  this.cap = capacity
+  this.limit = capacity
+  this.list = new DoubleList()
 }
 
 /**
@@ -13,10 +64,9 @@ var LRUCache = function (capacity) {
  */
 LRUCache.prototype.get = function (key) {
   if (this.cache.has(key)) {
-    const index = this.stack.indexOf(key)
-    this.stack.splice(index, 1)
-    this.stack.push(key)
-    return this.cache.get(key)
+    const val = this.cache.get(key).val
+    this.put(key, val)
+    return val
   }
   return -1
 }
@@ -27,16 +77,19 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
+  const node = new Node(key, value)
   if (this.cache.has(key)) {
-    const index = this.stack.indexOf(key)
-    this.stack.splice(index, 1)
-  } else if (this.cache.size === this.cap) {
-    const delKey = this.stack[0]
-    this.stack.shift()
-    this.cache.delete(delKey)
+    this.list.remove(this.cache.get(key))
+    this.list.addFirst(node)
+    this.cache.set(key, node)
+  } else {
+    if (this.cache.size === this.limit) {
+      const last = this.list.removeLast()
+      this.cache.delete(last.key)
+    }
+    this.list.addFirst(node)
+    this.cache.set(key, node)
   }
-  this.stack.push(key)
-  this.cache.set(key, value)
 }
 
 /**
