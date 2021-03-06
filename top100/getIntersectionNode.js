975da186760fce48f6ff38a25c39dcffaea91484
“windldream” <1508353859@qq.com>
2021-03-06 17:19:24 +0800

    160. 相交链表

---
+++ top100/getIntersectionNode.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (headA === null || headB === null) return null
+  let pA = headA
+  let pB = headB
+  while (pA !== pB) {
+    pA = pA === null ? headB : pA.next
+    pB = pB === null ? headA : pB.next
+  }
+  return pA
+}
