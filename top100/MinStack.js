53e6c9c2367bffcbce58eb28c311db6a0861dc66
“windldream” <1508353859@qq.com>
2021-03-06 16:51:28 +0800

    155. 最小栈

--- top100/MinStack.js
+++ top100/MinStack.js
@@ -8,8 +8,89 @@ class Node {
 /**
  * initialize your data structure here.
  */
+// var MinStack = function () {
+//   this.head = null
+// }
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function (x) {
+//   if (this.head === null) {
+//     this.head = new Node(x, x, null)
+//   } else {
+//     this.head = new Node(x, Math.min(x, this.head.min), this.head)
+//   }
+// }
+
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function () {
+//   this.head = this.head.next
+// }
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function () {
+//   return this.head.val
+// }
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function () {
+//   return this.head.min
+// }
+
+// var MinStack = function () {
+//   this.stack = []
+//   this.minStack = []
+// }
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MinStack.prototype.push = function (x) {
+//   this.stack.push(x)
+//   if (this.minStack.length) {
+//     if (x <= this.minStack[this.minStack.length - 1]) {
+//       this.minStack.push(x)
+//     }
+//   } else {
+//     this.minStack.push(x)
+//   }
+// }
+
+// /**
+//  * @return {void}
+//  */
+// MinStack.prototype.pop = function () {
+//   const val = this.stack.pop()
+//   if (val !== this.minStack[this.minStack.length - 1]) return
+//   this.minStack.pop()
+// }
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.top = function () {
+//   return this.stack[this.stack.length - 1]
+// }
+
+// /**
+//  * @return {number}
+//  */
+// MinStack.prototype.getMin = function () {
+//   return this.minStack[this.minStack.length - 1]
+// }
+
 var MinStack = function () {
-  this.head = null
+  this.stack = []
+  this.min = Infinity
 }
 
 /**
@@ -17,32 +98,34 @@ var MinStack = function () {
  * @return {void}
  */
 MinStack.prototype.push = function (x) {
-  if (this.head === null) {
-    this.head = new Node(x, x, null)
-  } else {
-    this.head = new Node(x, Math.min(x, this.head.min), this.head)
+  if (x <= this.min) {
+    this.stack.push(this.min)
+    this.min = x
   }
+  this.stack.push(x)
 }
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function () {
-  this.head = this.head.next
+  if (this.stack.pop() === this.min) {
+    this.min = this.stack.pop()
+  }
 }
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function () {
-  return this.head.val
+  return this.stack[this.stack.length - 1]
 }
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function () {
-  return this.head.min
+  return this.min
 }
 
 /**
