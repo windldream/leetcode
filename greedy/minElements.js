221fa3befb8e05d825e82b22386277fd7bcf03cd
“windldream” <1508353859@qq.com>
2021-03-07 14:47:37 +0800

    5698. 构成特定和需要添加的最少元素

---
+++ greedy/minElements.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} limit
+ * @param {number} goal
+ * @return {number}
+ */
+var minElements = function (nums, limit, goal) {
+  const sum = nums.reduce((prev, curr) => prev + curr, 0)
+  if (sum === goal) return 0
+
+  let diff
+  if (sum > goal) {
+    diff = sum - goal
+  } else {
+    diff = goal - sum
+  }
+  if (diff < limit) return 1
+  if (diff % limit === 0) return diff / limit
+  return ~~(diff / limit) + 1
+}
