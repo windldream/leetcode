c1f4f673a796100cf1683c6d1295321157fd717c
“windldream” <1508353859@qq.com>
2021-03-12 22:23:40 +0800

    331. 验证二叉树的前序序列化

--- daily/isValidSerialization.js
+++ daily/isValidSerialization.js
@@ -2,16 +2,26 @@
  * @param {string} preorder
  * @return {boolean}
  */
+// var isValidSerialization = function (preorder) {
+//   const list = preorder.split(',')
+//   const stack = []
+//   for (let i = 0; i < list.length; i++) {
+//     while (stack.length && stack[stack.length - 1] === '#' && list[i] === '#') {
+//       stack.pop()
+//       if (stack.length === 0) return false
+//       stack.pop()
+//     }
+//     stack.push(list[i])
+//   }
+//   return stack.length === 1 && stack[0] === '#'
+// }
 var isValidSerialization = function (preorder) {
   const list = preorder.split(',')
-  const stack = []
-  for (let i = 0; i < list.length; i++) {
-    while (stack.length && stack[stack.length - 1] === '#' && list[i] === '#') {
-      stack.pop()
-      if (stack.length === 0) return false
-      stack.pop()
-    }
-    stack.push(list[i])
+  let degree = 1
+  for (const node of list) {
+    degree -= 1
+    if (degree < 0) return false
+    if (node !== '#') degree += 2
   }
-  return stack.length === 1 && stack[0] === '#'
+  return degree === 0
 }
