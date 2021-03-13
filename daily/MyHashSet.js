f76b4ed7f699dfca5f5a0d5b759b2889a1fa9e66
“windldream” <1508353859@qq.com>
2021-03-13 14:56:22 +0800

    705. 设计哈希集合

---
+++ daily/MyHashSet.js
@@ -0,0 +1,42 @@
+/**
+ * Initialize your data structure here.
+ */
+var MyHashSet = function () {
+  this.set = []
+}
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {
+  if (this.set.includes(key)) return
+  this.set.push(key)
+}
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+  if (!this.set.includes(key)) return
+  const index = this.set.indexOf(key)
+  this.set.splice(index, 1)
+}
+
+/**
+ * Returns true if this set contains the specified element
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+  return this.set.includes(key)
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
