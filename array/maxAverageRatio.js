478ae1504f8154244d6e9efb78f53c4c6a0659d0
“windldream” <1508353859@qq.com>
2021-03-14 17:16:29 +0800

    5703. 最大平均通过率

---
+++ array/maxAverageRatio.js
@@ -0,0 +1,157 @@
+// 默认最小堆
+const defaultComparator = function (a, b) {
+  return a < b
+}
+
+class FastPriorityQueue {
+  constructor(comparator) {
+    if (!(this instanceof FastPriorityQueue)) return new FastPriorityQueue(comparator)
+    this.array = []
+    this.size = 0
+    this.compare = comparator || defaultComparator
+  }
+
+  add(myval) {
+    let i = this.size
+    this.array[this.size] = myval
+    this.size += 1
+    let p
+    let ap
+    while (i > 0) {
+      // 父节点
+      p = (i - 1) >> 1
+      ap = this.array[p]
+      // 停止上浮
+      if (!this.compare(myval, ap)) break
+      this.array[i] = ap
+      i = p
+    }
+    this.array[i] = myval
+  }
+
+  heapify(arr) {
+    this.array = arr
+    this.size = arr.length
+    for (let i = this.size >> 1; i >= 0; i--) {
+      this._percolateDown(i)
+    }
+  }
+
+  // 上浮
+  _percolateUp(i, force) {
+    const myval = this.array[i]
+    let p
+    let ap
+    while (i > 0) {
+      // 父节点
+      p = (i - 1) >> 1
+      ap = this.array[p]
+      if (!force && !this.compare(myval, ap)) break
+      this.array[i] = ap
+      i = p
+    }
+    this.array[i] = myval
+  }
+
+  // 下沉
+  _percolateDown(i) {
+    const size = this.size
+    const hsize = this.size >>> 1
+    const ai = this.array[i]
+    let l
+    let r
+    let bestc
+    while (i < hsize) {
+      // 左子节点
+      l = (i << 1) + 1
+      // 右子节点
+      r = l + 1
+      bestc = this.array[l]
+      if (r < size) {
+        if (this.compare(this.array[r], bestc)) {
+          l = r
+          bestc = this.array[r]
+        }
+      }
+      if (!this.compare(bestc, ai)) break
+      this.array[i] = bestc
+      i = l
+    }
+    this.array[i] = ai
+  }
+
+  // 删除索引为index的元素
+  _removeAt(index) {
+    if (index > this.size - 1 || index < 0) return undefined
+    // 让索引为index上浮到堆顶
+    this._percolateUp(index, true)
+    return this.poll()
+  }
+
+  // 删除逻辑等于myval的元素
+  remove(myval) {
+    for (let i = 0; i < this.size; i++) {
+      if (!this.compare(this.array[i], myval) && !this.compare(myval, this.array[i])) {
+        this._removeAt(i)
+        return true
+      }
+    }
+    return false
+  }
+
+  // 获取堆顶元素
+  peek() {
+    if (this.size === 0) return undefined
+    return this.array[0]
+  }
+
+  // 删除堆顶元素
+  poll() {
+    if (this.size === 0) return undefined
+    const ans = this.array[0]
+    if (this.size > 1) {
+      this.array[0] = this.array[--this.size]
+      this._percolateDown(0)
+    } else {
+      this.size -= 1
+    }
+    return ans
+  }
+
+  isEmpty() {
+    return this.size === 0
+  }
+}
+
+/**
+ * @param {number[][]} classes
+ * @param {number} extraStudents
+ * @return {number}
+ */
+var maxAverageRatio = function (classes, extraStudents) {
+  const pq = new FastPriorityQueue(
+    (a, b) => (a[0] + 1) / (a[1] + 1) + b[0] / b[1] > (b[0] + 1) / (b[1] + 1) + a[0] / a[1]
+  )
+  for (const [pass, total] of classes) {
+    pq.add([pass, total])
+  }
+  while (extraStudents > 0) {
+    let [pass, total] = pq.poll()
+    pq.add([pass + 1, total + 1])
+    extraStudents--
+  }
+
+  let ans = 0
+  for (const [pass, total] of pq.array) {
+    ans += pass / total
+  }
+  return ans / pq.size
+}
+
+maxAverageRatio(
+  [
+    [1, 2],
+    [3, 5]
+  ],
+  4
+)
