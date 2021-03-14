31f992c309a0db27cbbe6c87e87759d3dd7487cb
“windldream” <1508353859@qq.com>
2021-03-14 22:07:31 +0800

    287. 寻找重复数

--- top100/findDuplicate.js
+++ top100/findDuplicate.js
@@ -10,23 +10,40 @@
 //   }
 // }
 
+// var findDuplicate = function (nums) {
+//   const n = nums.length
+//   let l = 0
+//   let r = n - 1
+//   while (l <= r) {
+//     const mid = (l + r) >> 1
+//     let cnt = 0
+//     for (const num of nums) {
+//       if (num <= mid) cnt++
+//     }
+//     if (cnt > mid) {
+//       r = mid - 1
+//     } else {
+//       l = mid + 1
+//     }
+//   }
+//   return l
+// }
+
 var findDuplicate = function (nums) {
-  const n = nums.length
-  let l = 0
-  let r = n - 1
-  while (l <= r) {
-    const mid = (l + r) >> 1
-    let cnt = 0
-    for (const num of nums) {
-      if (num <= mid) cnt++
-    }
-    if (cnt > mid) {
-      r = mid - 1
-    } else {
-      l = mid + 1
+  let slow = 0
+  let fast = 0
+  while (true) {
+    slow = nums[slow]
+    fast = nums[nums[fast]]
+    if (slow === fast) {
+      fast = 0
+      while (nums[slow] !== nums[fast]) {
+        slow = nums[slow]
+        fast = nums[fast]
+      }
+      return nums[slow]
     }
   }
-  return l
 }
 
 findDuplicate([1, 3, 4, 2, 2])
