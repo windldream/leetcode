ff8b56624caac3d6633dcbe84b15cdab9d6cdc83
“windldream” <1508353859@qq.com>
2021-03-17 20:17:34 +0800

    312. 戳气球

--- top100/maxCoins.js
+++ top100/maxCoins.js
@@ -4,15 +4,19 @@
  */
 var maxCoins = function (nums) {
   const n = nums.length
-  nums = [1, ...nums, 1]
+  const list = [1, ...nums, 1]
   const dp = Array(n + 2)
     .fill(0)
     .map(() => Array(n + 2).fill(0))
   for (let len = 1; len <= n; len++) {
     for (let i = 1; len + i - 1 <= n; i++) {
       const j = len + i - 1
-      for (let k = i; k <= j; k++) {
-        dp[i][j] = Math.max(dp[i][j], dp[i][k - 1] + dp[k + 1][j] + nums[k] * nums[i - 1] * nums[j + 1])
+      if (len === 1) {
+        dp[i][j] = list[i - 1] * list[i] * list[i + 1]
+      } else {
+        for (let k = i; k <= j; k++) {
+          dp[i][j] = Math.max(dp[i][j], dp[i][k - 1] + dp[k + 1][j] + list[i - 1] * list[k] * list[j + 1])
+        }
       }
     }
   }
