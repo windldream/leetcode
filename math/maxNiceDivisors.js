214041956e1d01eafc577897162fef5462f49c3a
“windldream” <1508353859@qq.com>
2021-03-28 14:50:40 +0800

    5716. 好因子的最大数目

---
+++ math/maxNiceDivisors.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} primeFactors
+ * @return {number}
+ */
+var maxNiceDivisors = function (primeFactors) {
+  if (primeFactors <= 3) return primeFactors
+  const mod = BigInt(10 ** 9 + 7)
+
+  let ans = 0n
+  let quotient = BigInt(~~(primeFactors / 3))
+  let remainder = BigInt(primeFactors % 3)
+  if (remainder === 0n) {
+    ans = mypow(3n, quotient)
+  } else if (remainder === 1n) {
+    ans = mypow(3n, quotient - 1n) * 4n
+  } else {
+    ans = mypow(3n, quotient) * 2n
+  }
+  return ans % mod
+
+  function mypow(x, n) {
+    let ans = 1n
+    while (n) {
+      if (n % 2n) {
+        ans *= x
+        ans %= mod
+      }
+      x *= x
+      x %= mod
+      n = n >> 1n
+    }
+    return ans
+  }
+}
