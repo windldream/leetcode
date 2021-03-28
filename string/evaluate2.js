e09d351cb1badca2053f7f00410cf971c80b9967
“windldream” <1508353859@qq.com>
2021-03-28 13:37:34 +0800

    5714. 替换字符串中的括号内容

--- string/evaluate2.js
+++ string/evaluate2.js
@@ -6,9 +6,6 @@
 var evaluate = function (s, knowledge) {
   const knowledgeMap = new Map()
   for (const [key, val] of knowledge) knowledgeMap.set(key, val)
-  const reg = /\([a-z]+\)/g
-  return s.replace(reg, (match) => {
-    const key = match.substring(1, match.length - 1)
-    return knowledgeMap.has(key) ? knowledgeMap.get(key) : '?'
-  })
+  const reg = /\(([a-z]+)\)/g
+  return s.replace(reg, (match, p1) => (knowledgeMap.has(p1) ? knowledgeMap.get(p1) : '?'))
 }
