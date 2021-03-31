a802a21a42e5e31f65394e58f2747351a55adc17
“windldream” <1508353859@qq.com>
2021-03-31 19:11:27 +0800

    剑指 Offer 13. 机器人的运动范围

--- offer/movingCount.js
+++ offer/movingCount.js
@@ -18,15 +18,23 @@ var movingCount = function (m, n, k) {
     dfs(i, j + 1)
   }
 
+  // function sum(i, j) {
+  //   let ans = 0
+  //   while (i) {
+  //     ans += i % 10
+  //     i = ~~(i / 10)
+  //   }
+  //   while (j) {
+  //     ans += j % 10
+  //     j = ~~(j / 10)
+  //   }
+  //   return ans
+  // }
+
   function sum(i, j) {
     let ans = 0
-    while (i) {
-      ans += i % 10
-      i = ~~(i / 10)
-    }
-    while (j) {
-      ans += j % 10
-      j = ~~(j / 10)
+    for (let num of i + '' + j) {
+      ans += +num
     }
     return ans
   }
