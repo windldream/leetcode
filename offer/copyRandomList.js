3919a6a04ca8372f9b59771059cf459a7e43c660
“windldream” <1508353859@qq.com>
2021-04-03 18:50:23 +0800

    剑指 Offer 35. 复杂链表的复制

--- offer/copyRandomList.js
+++ offer/copyRandomList.js
@@ -11,20 +11,35 @@
  * @param {Node} head
  * @return {Node}
  */
+// var copyRandomList = function (head) {
+//   const cache = new Map()
+//   let tmp = head
+//   while (tmp) {
+//     const clone = new Node(tmp.val)
+//     cache.set(tmp, clone)
+//     tmp = tmp.next
+//   }
+
+//   let cur = head
+//   while (cur) {
+//     cache.get(cur).next = cur.next ? cache.get(cur.next) : null
+//     cache.get(cur).random = cur.random ? cache.get(cur.random) : null
+//     cur = cur.next
+//   }
+//   return cache.get(head)
+// }
+
 var copyRandomList = function (head) {
   const cache = new Map()
-  let tmp = head
-  while (tmp) {
-    const clone = new Node(tmp.val)
-    cache.set(tmp, clone)
-    tmp = tmp.next
-  }
+  return dfs(head)
 
-  let cur = head
-  while (cur) {
-    cache.get(cur).next = cur.next ? cache.get(cur.next) : null
-    cache.get(cur).random = cur.random ? cache.get(cur.random) : null
-    cur = cur.next
+  function dfs(head) {
+    if (head === null) return head
+    if (cache.has(head)) return cache.get(head)
+    const clone = new Node(head.val)
+    cache.set(head, clone)
+    clone.next = dfs(head.next)
+    clone.random = dfs(head.random)
+    return clone
   }
-  return cache.get(head)
 }
