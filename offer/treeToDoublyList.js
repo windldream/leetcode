4b91ab6960917424b4fa72191e92f82683a250ac
“windldream” <1508353859@qq.com>
2021-04-06 07:26:51 +0800

    剑指 Offer 36. 二叉搜索树与双向链表

--- offer/treeToDoublyList.js
+++ offer/treeToDoublyList.js
@@ -10,25 +10,45 @@
  * @param {Node} root
  * @return {Node}
  */
+// var treeToDoublyList = function (root) {
+//   if (root === null) return null
+//   const ans = []
+//   inorder(root)
+//   const dummy = new Node()
+//   let cur = dummy
+//   for (let i = 0; i < ans.length; i++) {
+//     cur.right = new Node(ans[i])
+//     cur.right.left = cur
+//     cur = cur.right
+//   }
+//   cur.right = dummy.right
+//   dummy.right.left = cur
+//   return dummy.right
+
+//   function inorder(root) {
+//     if (root === null) return
+//     inorder(root.left)
+//     ans.push(root.val)
+//     inorder(root.right)
+//   }
+// }
+
 var treeToDoublyList = function (root) {
   if (root === null) return null
-  const ans = []
+  let pre = null
+  let head = null
   inorder(root)
-  const dummy = new Node()
-  let cur = dummy
-  for (let i = 0; i < ans.length; i++) {
-    cur.right = new Node(ans[i])
-    cur.right.left = cur
-    cur = cur.right
-  }
-  cur.right = dummy.right
-  dummy.right.left = cur
-  return dummy.right
+  head.left = pre
+  pre.right = head
+  return head
 
-  function inorder(root) {
-    if (root === null) return
-    inorder(root.left)
-    ans.push(root.val)
-    inorder(root.right)
+  function inorder(cur) {
+    if (cur === null) return
+    inorder(cur.left)
+    if (pre) pre.right = cur
+    else head = cur
+    cur.left = pre
+    pre = cur
+    inorder(cur.right)
   }
 }
