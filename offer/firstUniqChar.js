b424f9481b5dd7aacce6c70d781eb6e890370f9d
“windldream” <1508353859@qq.com>
2021-04-10 05:19:19 +0800

    剑指 Offer 50. 第一个只出现一次的字符

---
+++ offer/firstUniqChar.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var firstUniqChar = function (s) {
+  for (const c of s) {
+    if (s.indexOf(c) == s.lastIndexOf(c)) return c
+  }
+  return ' '
+}
