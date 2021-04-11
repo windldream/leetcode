59b4bb6978f64e1fb2d0daf5b45029165892e1d6
“windldream” <1508353859@qq.com>
2021-04-12 07:39:14 +0800

    剑指 Offer 61. 扑克牌中的顺子

--- offer/isStraight.js
+++ offer/isStraight.js
@@ -10,12 +10,10 @@ var isStraight = function (nums) {
     start++
     wang++
   }
-  let prev = nums[start++]
-  for (let i = start; i < nums.length; i++) {
-    const diff = nums[i] - prev
+  for (let i = start + 1; i < nums.length; i++) {
+    const diff = nums[i] - nums[i - 1]
     if (diff > 3 || diff === 0 || wang < diff - 1) return false
     wang -= diff - 1
-    prev = nums[i]
   }
   return true
 }
