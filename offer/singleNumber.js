8c90ada6d5e3f6c92ba3465b5e2ad41119071df1
“windldream” <1508353859@qq.com>
2021-04-11 18:11:59 +0800

    剑指 Offer 56 - II. 数组中数字出现的次数 II

--- offer/singleNumber.js
+++ offer/singleNumber.js
@@ -1,19 +1,23 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// const singleNumber = function (nums) {
+//   let ans = 0
+//   for (let i = 0; i < 31; i++) {
+//     const mask = 1 << i
+//     let count = 0
+//     // 使用mask分组
+//     for (const num of nums) {
+//       if (mask & num) count += 1
+//     }
+//     if (count % 3 !== 0) {
+//       ans = ans | mask
+//     }
+//   }
+//   return ans
+// }
+
 const singleNumber = function (nums) {
-  let ans = 0
-  for (let i = 0; i < 31; i++) {
-    const mask = 1 << i
-    let count = 0
-    // 使用mask分组
-    for (const num of nums) {
-      if (mask & num) count += 1
-    }
-    if (count % 3 !== 0) {
-      ans = ans | mask
-    }
-  }
-  return ans
+  return ([...new Set(nums)].reduce((prev, curr) => prev + curr, 0) * 3 - nums.reduce((prev, curr) => prev + curr)) / 2
 }
