8ccc059f0dc09ecb93a2c4785f546e5a8c4b94c8
“windldream” <1508353859@qq.com>
2021-04-12 21:20:28 +0800

    剑指 Offer 63. 股票的最大利润

--- offer/maxProfit.js
+++ offer/maxProfit.js
@@ -2,15 +2,29 @@
  * @param {number[]} prices
  * @return {number}
  */
+// var maxProfit = function (prices) {
+//   const stack = []
+//   let max = 0
+//   for (const price of prices) {
+//     while (stack.length && stack[stack.length - 1] > price) {
+//       const last = stack.pop()
+//     }
+//     stack.push(price)
+//     if (stack.length > 1) max = Math.max(max, stack[stack.length - 1] - stack[0])
+//   }
+//   return max
+// }
+
 var maxProfit = function (prices) {
-  const stack = []
-  let max = 0
-  for (const price of prices) {
-    while (stack.length && stack[stack.length - 1] > price) {
-      const last = stack.pop()
+  const n = prices.length
+  let min = prices[0]
+  let ans = 0
+  for (let i = 1; i < n; i++) {
+    if (prices[i] < min) {
+      min = prices[i]
+    } else {
+      ans = Math.max(ans, prices[i] - min)
     }
-    stack.push(price)
-    if (stack.length > 1) max = Math.max(max, stack[stack.length - 1] - stack[0])
   }
-  return max
+  return ans
 }
