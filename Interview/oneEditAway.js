6c9a8c66b8fd30dd75400c3fbe704ae0ddecf656
“windldream” <1508353859@qq.com>
2021-04-14 07:05:49 +0800

    面试题 01.05. 一次编辑

---
+++ Interview/oneEditAway.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} first
+ * @param {string} second
+ * @return {boolean}
+ */
+var oneEditAway = function (first, second) {
+  const m = first.length
+  const n = second.length
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(Infinity))
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i
+  }
+  for (let i = 0; i <= n; i++) {
+    dp[0][i] = i
+  }
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (first[i - 1] === second[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1]
+      } else {
+        // 1.删除
+        // 2.插入
+        // 3.替换
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+      }
+    }
+  }
+  return dp[m][n] <= 1
+}
