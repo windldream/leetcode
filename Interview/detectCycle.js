c485c002eff9eb76b0ca25264361c88cd1d12e46
“windldream” <1508353859@qq.com>
2021-04-17 16:26:43 +0800

    面试题 02.08. 环路检测

---
+++ Interview/detectCycle.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+    if (fast === slow) {
+      fast = head
+      while (slow !== fast) {
+        slow = slow.next
+        fast = fast.next
+      }
+      return fast
+    }
+  }
+  return null
+}
