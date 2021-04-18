90187723bc34ea5eb2a4233a30d1216aa618d196
“windldream” <1508353859@qq.com>
2021-04-19 06:39:45 +0800

    27. 移除元素

---
+++ daily/removeElement.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  let j = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) nums[j++] = nums[i]
+  }
+  return j
+}
+
+removeElement([3, 2, 2, 3], 3)
