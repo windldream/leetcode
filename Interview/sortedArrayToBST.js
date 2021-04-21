5143a3e517988e6b4f25e37c0e9604c24cc75903
“windldream” <1508353859@qq.com>
2021-04-21 19:31:36 +0800

    面试题 04.02. 最小高度树

---
+++ Interview/sortedArrayToBST.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+var sortedArrayToBST = function (nums) {
+  const n = nums.length
+  if (n === 0) return null
+  const mid = n >> 1
+  const root = new TreeNode(nums[mid])
+  root.left = sortedArrayToBST(nums.slice(0, mid))
+  root.right = sortedArrayToBST(nums.slice(mid + 1))
+  return root
+}
