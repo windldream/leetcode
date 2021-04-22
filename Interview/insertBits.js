c11f94133326c031f4def1a3d14c3e8c858d115e
“windldream” <1508353859@qq.com>
2021-04-23 07:33:51 +0800

    面试题 05.01. 插入

---
+++ Interview/insertBits.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} N
+ * @param {number} M
+ * @param {number} i
+ * @param {number} j
+ * @return {number}
+ */
+var insertBits = function (N, M, i, j) {
+  for (let k = i; k <= j; k++) {
+    if (N & (1 << k)) {
+      N -= 1 << k
+    }
+  }
+  return N + (M << i)
+}
+
+insertBits(1024, 19, 2, 6)
+
+//
