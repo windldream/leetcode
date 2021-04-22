df25dc63826576f91249e6a44fc408c942f7c3b3
“windldream” <1508353859@qq.com>
2021-04-22 18:58:39 +0800

    面试题 04.08. 首个共同祖先

---
+++ Interview/lowestCommonAncestor.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (root === p || root === q || root === null) return root
+  const l = lowestCommonAncestor(root.left, p, q)
+  const r = lowestCommonAncestor(root.right, p, q)
+  if (l && r) return root
+  return l || r || null
+}
