c9ba2a139b9a799be1f8143163528e0dbb95bd83
“windldream” <1508353859@qq.com>
2021-04-24 21:04:55 +0800

    面试题 05.02. 二进制数转字符串

---
+++ Interview/printBin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var printBin = function (num) {
+  let ans = '0.'
+  let i = 30
+  while (num > 0 && i--) {
+    num *= 2
+    if (num >= 1) {
+      ans += '1'
+      --num
+    } else {
+      ans += '0'
+    }
+  }
+  return num !== 0 ? 'ERROR' : ans
+}
+
+//
