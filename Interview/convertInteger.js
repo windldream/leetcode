3696c88078bf71f3f109b4f017b4d8d5c413d5ba
“windldream” <1508353859@qq.com>
2021-04-26 06:54:32 +0800

    面试题 05.06. 整数转换

--- Interview/convertInteger.js
+++ Interview/convertInteger.js
@@ -3,15 +3,25 @@
  * @param {number} B
  * @return {number}
  */
+// var convertInteger = function (A, B) {
+//   let i = 0
+//   let cnt = 0
+//   while (i++ < 32) {
+//     if (((A >> i) ^ (B >> i)) & 1) {
+//       cnt++
+//     }
+//   }
+//   return cnt
+// }
+
 var convertInteger = function (A, B) {
-  let i = 0
-  let cnt = 0
-  while (i++ < 32) {
-    if (((A >> i) ^ (B >> i)) & 1) {
-      cnt++
-    }
+  let num = A ^ B
+  let ans = 0
+  while (num) {
+    num = num & (num - 1)
+    ans++
   }
-  return cnt
+  return ans
 }
 
 convertInteger(29, 15)
