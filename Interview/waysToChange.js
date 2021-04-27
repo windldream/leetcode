b0e972b49b8a2626740845822ae5e50e67578f8f
“windldream” <1508353859@qq.com>
2021-04-28 05:52:14 +0800

    面试题 08.11. 硬币

---
+++ Interview/waysToChange.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var waysToChange = function (n) {
+  const mod = 10 ** 9 + 7
+  const coins = [25, 10, 5, 1]
+  const dp = Array(n + 1).fill(0)
+  dp[0] = 1
+  for (const coin of coins) {
+    for (let i = coin; i <= n; i++) {
+      dp[i] = (dp[i] + dp[i - coin]) % mod
+    }
+  }
+  return dp[n]
+}
+
+//
+
+waysToChange(10)
