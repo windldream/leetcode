a4c65574a78ef12053405b4c4fafbeedf0e7884c
“windldream” <1508353859@qq.com>
2021-05-14 23:10:09 +0800

    面试题 16.20. T9键盘

---
+++ Interview/getValidT9Words.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} num
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var getValidT9Words = function (num, words) {
+  const nums = ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  for (let i = 0; i < num.length; i++) {
+    const chars = nums[num[i] - 2]
+    words = words.filter((word) => chars.includes(word[i]))
+  }
+  return words
+}
+
+getValidT9Words('8733', ['tree', 'used'])
