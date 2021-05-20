36e7f899aee0ed78de8f97ad2cd4f95dc253f69d
“windldream” <1508353859@qq.com>
2021-05-21 07:36:25 +0800

    1035. 不相交的线

---
+++ daily/maxUncrossedLines.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxUncrossedLines = function (nums1, nums2) {
+  const m = nums1.length
+  const n = nums2.length
+  const dp = Array(m + 1)
+    .fill(0)
+    .map(() => Array(n + 1).fill(0))
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (nums1[i - 1] === nums2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[m][n]
+}
