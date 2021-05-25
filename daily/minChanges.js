a2b4ed13b6af8a520da25cf49ca9e7a79edbcea1
“windldream” <1508353859@qq.com>
2021-05-25 20:37:23 +0800

    1787. 使所有区间的异或结果为零

--- daily/minChanges.js
+++ daily/minChanges.js
@@ -21,12 +21,16 @@ var minChanges = function (nums, k) {
   dp[0] = 0
   for (let i = 0; i < k; i++) {
     const low = Math.min(...dp)
+    // 第i列 整列替换
     const ndp = Array(1 << 10).fill(low + sizes[i])
+    // 修改第i列部分数
     for (let j = 0; j < 1 << 10; j++) {
       if (dp[j] === Infinity) continue
+      // 第i列为num的数不替换，剩余的数替换次数为sizes[i] - freq
       for (const [num, freq] of groups[i]) {
         const next = num ^ j
-        // 修改为num
+        // ndp[next] 表示前i列异或值为next的最小修改次数
+        // dp[j] 表示前i-1列异或值为j的最小修改次数
         ndp[next] = Math.min(ndp[next], dp[j] + sizes[i] - freq)
       }
     }
