a05feef2f83b16afdae0f4d4d577ae1fc578483d
“windldream” <1508353859@qq.com>
2021-06-22 21:36:15 +0800

    1903. 字符串中的最大奇数

--- string/largestOddNumber.js
+++ string/largestOddNumber.js
@@ -2,14 +2,24 @@
  * @param {string} num
  * @return {string}
  */
+// var largestOddNumber = function (num) {
+//   let maxStr = ''
+//   let preStr = ''
+//   for (const c of num) {
+//     if (Number(c) & 1) {
+//       maxStr = preStr + c
+//     }
+//     preStr += c
+//   }
+//   return maxStr
+// }
+
 var largestOddNumber = function (num) {
-  let maxStr = ''
-  let preStr = ''
-  for (const c of num) {
-    if (Number(c) & 1) {
-      maxStr = preStr + c
+  let n = num.length
+  for (let i = n - 1; i >= 0; i--) {
+    if (Number(num[i]) & 1) {
+      return num.slice(0, i + 1)
     }
-    preStr += c
   }
-  return maxStr
+  return ''
 }
