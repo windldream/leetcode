0451afe086960352f95cee17e3e418b56e477f43
“windldream” <1508353859@qq.com>
2021-07-01 20:25:40 +0800

    LCP 07. 传递信息

---
+++ daily/numWays2.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @param {number[][]} relation
+ * @param {number} k
+ * @return {number}
+ */
+var numWays = function (n, relation, k) {
+  const g = new Map()
+  for (const [u, v] of relation) {
+    if (!g.has(u)) g.set(u, new Set())
+    g.get(u).add(v)
+  }
+
+  const dp = Array(k + 1)
+    .fill(0)
+    .map(() => Array(n).fill(0))
+  for (const next of g.get(0)) {
+    dp[1][next] = 1
+  }
+
+  for (let i = 2; i <= k; i++) {
+    for (let j = 0; j < n; j++) {
+      for (let s = 0; s < n; s++) {
+        if (s === j) continue
+        if (g.has(s) && g.get(s).has(j)) {
+          dp[i][j] += dp[i - 1][s]
+        }
+      }
+    }
+  }
+  return dp[k][n - 1]
+}
