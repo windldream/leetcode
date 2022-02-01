ac0e08055dee76ead87317e756ab7b7a2e813523
“windldream” <1508353859@qq.com>
2022-02-01 11:43:37 +0800

    1914. 循环轮转矩阵

--- array/rotateGrid.js
+++ array/rotateGrid.js
@@ -12,47 +12,55 @@ var rotateGrid = function (grid, k) {
     const data = []
     let index = 0
 
-    // 从左往右
-    for (let offset = i; offset < n - i - 1; offset++) {
-      data[index++] = grid[i][offset]
-    }
-
-    // 从上往下
-    for (let offset = i; offset < m - i - 1; offset++) {
-      data[index++] = grid[offset][n - i - 1]
-    }
-
-    // 从右往左
-    for (let offset = n - i - 1; offset > i; offset--) {
-      data[index++] = grid[m - i - 1][offset]
-    }
-
-    // 从下往上
-    for (let offset = m - i - 1; offset > i; offset--) {
-      data[index++] = grid[offset][i]
-    }
+    InitData(data, index, i, grid)
 
     const rotateData = rotateVector(data, k)
     index = 0
 
-    for (let offset = i; offset < n - i - 1; offset++) {
-      grid[i][offset] = rotateData[index++]
-    }
-
-    for (let offset = i; offset < m - i - 1; offset++) {
-      grid[offset][n - i - 1] = rotateData[index++]
-    }
-
-    for (let offset = n - i - 1; offset > i; offset--) {
-      grid[m - i - 1][offset] = rotateData[index++]
-    }
-
-    for (let offset = m - i - 1; offset > i; offset--) {
-      grid[offset][i] = rotateData[index++]
-    }
+    reverseData(rotateData, index, i, grid)
   }
 
   return grid
+
+  function reverseData(rotateData, index, layer, grid) {
+    for (let offset = layer; offset < n - layer - 1; offset++) {
+      grid[layer][offset] = rotateData[index++]
+    }
+
+    for (let offset = layer; offset < m - layer - 1; offset++) {
+      grid[offset][n - layer - 1] = rotateData[index++]
+    }
+
+    for (let offset = n - layer - 1; offset > layer; offset--) {
+      grid[m - layer - 1][offset] = rotateData[index++]
+    }
+
+    for (let offset = m - layer - 1; offset > layer; offset--) {
+      grid[offset][layer] = rotateData[index++]
+    }
+  }
+
+  function InitData(data, index, layer, grid) {
+    // 从左往右
+    for (let offset = layer; offset < n - layer - 1; offset++) {
+      data[index++] = grid[layer][offset]
+    }
+
+    // 从上往下
+    for (let offset = layer; offset < m - layer - 1; offset++) {
+      data[index++] = grid[offset][n - layer - 1]
+    }
+
+    // 从右往左
+    for (let offset = n - layer - 1; offset > layer; offset--) {
+      data[index++] = grid[m - layer - 1][offset]
+    }
+
+    // 从下往上
+    for (let offset = m - layer - 1; offset > layer; offset--) {
+      data[index++] = grid[offset][layer]
+    }
+  }
 
   function rotateVector(vector, offset) {
     offset = offset % vector.length
