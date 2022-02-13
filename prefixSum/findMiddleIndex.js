368ec0724998adfdfc5b06d5300205f9ecdaabcf
“windldream” <1508353859@qq.com>
2022-02-13 22:22:46 +0800

    1991. 找到数组的中间位置

---
+++ prefixSum/findMiddleIndex.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMiddleIndex = function (nums) {
+  const n = nums.length
+  const prefixSum = Array(n + 1).fill(0)
+
+  for (let i = 0; i < n; i++) {
+    prefixSum[i + 1] += prefixSum[i] + nums[i]
+  }
+
+  for (let i = 0; i < n; i++) {
+    const left = prefixSum[i]
+    const right = prefixSum[n] - prefixSum[i + 1]
+
+    if (left === right) return i
+  }
+
+  return -1
+}
