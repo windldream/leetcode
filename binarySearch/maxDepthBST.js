898917289ba92b8eef2090417ce9542c0f7b5760
“windldream” <1508353859@qq.com>
2022-02-26 23:11:30 +0800

    2037. 使每位学生都有座位的最少移动次数

---
+++ binarySearch/maxDepthBST.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} order
+ * @return {number}
+ */
+var maxDepthBST = function (order) {
+  let depth = 0
+  const root = function buildTree() {}
+
+  function Tree() {
+    this.left = null
+    this.right = null
+  }
+}
